686ba5cacd66b2ec9ce5bfb59aa41753fe4c5e31
hippo91 <guillaume.peillex@gmail.com>
2015-04-08 23:22:32 +0200

    Ajout de la méthode pour calculer la pression (Newton) dans le schéma VNR pour les formulations en v-eœ

--- xvof/element/element.py
+++ xvof/element/element.py
@@ -9,7 +9,7 @@ Classe de base définissant un élément
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 from abc import abstractmethod
 import numpy as np
-from miscellaneous import *
+from xvof.miscellaneous import *
 
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 ####### DEFINITION DES CLASSES & FONCTIONS  ###############
@@ -167,3 +167,107 @@ class Element(object):
         message += "==> vitesse du son à t+dt = {}\n".\
             format(self.cson_t_plus_dt)
         print message
+
+    def calculer_nouvo_pression(self):
+        """
+        Algorithme de Newton-Raphson pour déterminer le couple
+        energie/pression au pas de temps suivant
+        Formulation v-e
+
+        TEST UNITAIRE
+        >>> from xvof.miscellaneous import *
+        >>> from xvof.equationsofstate import MieGruneisen
+        >>> ee = MieGruneisen()
+        >>> num_props = numerical_props(0.2, 1.0)
+        >>> mat_props = material_props(1.0e+05, 0.0, 8129., ee)
+        >>> geom_props = geometrical_props(1.0e-06)
+        >>> props = properties(num_props, mat_props, geom_props)
+        >>> my_elem = Element(props, 123, 2.5e-03)
+        >>> my_elem._rho_t_plus_dt = 9000.0
+        >>> my_elem.calculer_nouvo_pression()
+        """
+        delta_v = 1. / self.rho_t_plus_dt - 1. / self.rho_t
+        pression_t = self.pression_t + 2. * self.pseudo
+        # Variable du Newton
+        nrj_i = self.nrj_t
+        # Critère de convergence
+        convergence = False
+        # Nombre d'itérations
+        nit = 0
+        #
+
+        def calcul_F_et_dF(enerj):
+            """
+            Fonction à annuler et sa dérivée pour le schéma VNR
+            Formulation v-e
+            """
+            (p_i, dpsurde, vitson) = \
+            self.proprietes.material.eos.solve_ve(1. / self.rho_t_plus_dt, enerj)
+            # Fonction à annuler
+            func = enerj + p_i * delta_v / 2. + pression_t * delta_v / 2. -\
+                self.nrj_t
+            # Dérivée de la fonction à annuler
+            dfunc = 1 + dpsurde * delta_v / 2.
+            return (func, dfunc)
+        #
+        (fi, dfisurde) = calcul_F_et_dF(nrj_i)
+        #
+        while(not convergence and (nit < 100)):
+            # Correction
+            nrj_iplus1 = nrj_i - fi / dfisurde
+            nit += 1
+            if(abs(fi) < 1e-09):
+                convergence = True
+                res_nrj = nrj_i
+                res_pression_t_plus_dt, dummy, res_cson = \
+                self.proprietes.material.eos.solve_ve(
+                    1. / self.rho_t_plus_dt, res_nrj)
+                break
+            # Incrémentation
+            nrj_i = nrj_iplus1
+            #
+            (fi, dfisurde) = calcul_F_et_dF(nrj_i)
+            if(abs(dfisurde) < 1.e-09):
+                print "Sortie du NR par manque de pente :-)"
+                convergence = True
+                res_nrj = nrj_i
+                res_pression_t_plus_dt = p_i
+                res_cson = cel_son
+                break
+        if(nit == 100):
+            print "Erreur de convergence du NR"
+            print "fi=", fi
+            print "nit=", nit
+            exit(255)
+        return res_nrj, res_pression_t_plus_dt, res_cson
+
+    #------------------------------------------------------------
+    # DEFINITIONS DES METHODES VIRTUELLES
+    #------------------------------------------------------------
+    @abstractmethod
+    def calculer_nouvo_taille(self):
+        """
+        Calcul de la nouvelle taille (longueur, aire, volume) de l'élément
+        """
+
+    @abstractmethod
+    def calculer_nouvo_densite(self):
+        """
+        Calcul de la densité à l'instant t+dt basé sur
+        la conservation de la masse
+        """
+
+    @abstractmethod
+    def calculer_nouvo_pseudo(self):
+        """
+        Calcul de la nouvelle pseudo
+        """
+
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#######          PROGRAMME PRINCIPAL        ###############
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+if __name__ == "__main__":
+    import doctest
+    testres = doctest.testmod(verbose=0)
+    if(testres[0] == 0):
+        print "TESTS UNITAIRES : OK"
