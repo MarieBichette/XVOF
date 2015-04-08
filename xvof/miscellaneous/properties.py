f74ddcab82413c634099ec8fca1a12c71708d9bf
hippo91 <guillaume.peillex@gmail.com>
2015-04-08 19:21:41 +0200

    Création du package miscellaneous Création des propriétés comme namedtuple

---
+++ xvof/miscellaneous/properties.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2.7
+# -*- coding: iso-8859-1 -*-
+"""
+Divers choses telles que les propriétés
+"""
+
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+############ IMPORTATIONS DIVERSES  ####################
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+from collections import namedtuple
+
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+####### DEFINITION DES CLASSES & FONCTIONS  ###############
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+numerical_props = namedtuple("numerical_props", ["a_pseudo", "b_pseudo"])
+
+geometrical_props = namedtuple("geometrical_props", ["section"])
+
+material_props = namedtuple("material_props",
+                    ["pression_init", "energie_init", "rho_init", "eos"])
+
+properties = namedtuple("properties", ["numeric", "material", "geometric"])
