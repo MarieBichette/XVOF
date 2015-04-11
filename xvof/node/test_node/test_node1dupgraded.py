f6f8a0da25bc29be3386ead76ce721256f4fef1f
hippo91 <guillaume.peillex@gmail.com>
2015-04-11 18:08:34 +0200

    Ajout du module de test pour le module Node1dUpgraded

---
+++ xvof/node/test_node/test_node1dupgraded.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2.7
+# -*- coding: iso-8859-1 -*-
+"""
+Classe de test du module node1dupgraded
+"""
+import xvof.node.node1dupgraded as nd1dup
+import numpy as np
+import unittest
+
+
+class Node1dUpgradedTest(unittest.TestCase):
+    """
+    Test case utilisé pour test les fonctions du module 'Node1dUpgraded'
+    """
+    def setUp(self):
+        """
+        Préparation des tests unitaires
+        """
+
