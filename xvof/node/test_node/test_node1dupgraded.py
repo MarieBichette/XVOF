97d2d7d2c595f5b0cf74e7d82fd35f64a8793523
hippo91 <guillaume.peillex@gmail.com>
2015-04-12 10:46:18 +0200

    Codage des fonctions de tests du calcul des nouvelles forces et nouvelles vitesses

--- xvof/node/test_node/test_node1dupgraded.py
+++ xvof/node/test_node/test_node1dupgraded.py
@@ -21,13 +21,16 @@ class Node1dUpgradedTest(unittest.TestCase):
             def __init__(self, poz, pressure, pseudo, masse):
                 self.coord = poz
                 self.pression_t_plus_dt = pressure
+                self._pression_t_plus_dt_enrichi = pressure * 0.5
                 self.pseudo = pseudo
+                self._pseudo_plus_un_demi_enrichi = pseudo * 0.5
                 self.masse = masse
         self.elem_gauche = element(np.array([-0.5]), 2.5e+09, 1.0e+09, 3. / 4.)
         self.elem_droite = element(np.array([0.5]), 1.0e+09, 0.5e+09, 1. / 4.)
         my_node = nd1d.Node1d(1, section=1.0e-06)
         my_node.elements_voisins = [self.elem_droite, self.elem_gauche]
         self.my_node = nd1dup.Node1dUpgraded(my_node)
+        self.my_node.calculer_masse_wilkins()
 
     def test_position_relative(self):
         """
@@ -44,15 +47,41 @@ class Node1dUpgradedTest(unittest.TestCase):
         """
         Test de la fonction Node1dUpgraded.calculer_nouvo_force
         """
+        # Noeud à droite de la discontinuité
+        self.my_node.position_relative = -1
         self.my_node.calculer_nouvo_force()
         np.testing.assert_array_equal(self.my_node.force_classique,
-                                      np.array([400.]))
+                                      np.array([2000.]))
+        np.testing.assert_array_equal(self.my_node.force_enrichi,
+                                      np.array([2750.]))
+        # Noeud à gauche de la discontinuité
+        self.my_node.position_relative = 1
+        self.my_node.calculer_nouvo_force()
+        np.testing.assert_array_equal(self.my_node.force_classique,
+                                      np.array([2000.]))
+        np.testing.assert_array_equal(self.my_node.force_enrichi,
+                                      np.array([-3250.]))
 
     def test_calculer_nouvo_vitesse(self):
         """
         Test de la méthode Node1dUpgraded.calculer_nouvo_vitesse
         """
-        self.my_node.calculer_masse_wilkins()
+        #  Noeud à droite de la discontinuité
+        self.my_node.position_relative = -1
+        self.my_node.calculer_nouvo_force()
+        self.my_node.calculer_nouvo_vitesse(1.0e-01)
+        np.testing.assert_array_equal(self.my_node.upundemi_classique,
+                                      np.array([400.0]))
+        np.testing.assert_array_equal(self.my_node.upundemi_enrichi,
+                                      np.array([550.0]))
+        # Noeud à gauche de la discontinuité
+        self.my_node.position_relative = 1
+        self.my_node.calculer_nouvo_force()
+        self.my_node.calculer_nouvo_vitesse(1.0e-01)
+        np.testing.assert_array_equal(self.my_node.upundemi_classique,
+                                      np.array([400.0]))
+        np.testing.assert_array_equal(self.my_node.upundemi_enrichi,
+                                      np.array([-650.0]))
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ############ PROGRAMME PRINCIPAL ####################
