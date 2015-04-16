5c129f64a75a14d6e0b0ba5270b7d2ed7618e825
hippo91 <guillaume.peillex@gmail.com>
2015-04-16 22:18:51 +0200

    Suppression des tests unitaires de type doctest car ils sont remplacés par les tests de type unittest

--- xvof/element/element1d.py
+++ xvof/element/element1d.py
@@ -149,28 +149,6 @@ class Element1d(Element):
         Calcul du triplet energie, pression, vitesse du son
         au pas de temps suivant
         Formulation v-e
-
-        TEST UNITAIRE
-        >>> import numpy as np
-        >>> from xvof.node import Node1d
-        >>> from xvof.miscellaneous import *
-        >>> from xvof.equationsofstate import MieGruneisen
-        >>> ee = MieGruneisen()
-        >>> num_props = numerical_props(0.2, 1.0, 0.35)
-        >>> mat_props = material_props(1.0e+05, 0.0, 8129., ee)
-        >>> geom_props = geometrical_props(1.0e-06)
-        >>> props = properties(num_props, mat_props, geom_props)
-        >>> noda = Node1d(1, poz_init=np.array([-1.0e-03]))
-        >>> nodb = Node1d(2, poz_init=np.array([1.5e-03]))
-        >>> my_elem = Element1d(props, 123, [noda, nodb])
-        >>> my_elem._rho_t_plus_dt = 9000.0
-        >>> my_elem.calculer_nouvo_pression()
-        >>> print my_elem.nrj_t_plus_dt/1e+05
-        1.03378424404
-        >>> print my_elem.pression_t_plus_dt/1.0e+09
-        17.3667631638
-        >>> print my_elem.cson_t_plus_dt/1.0e+03
-        4.89803134405
         """
         self._nrj_t_plus_dt, self._pression_t_plus_dt, self._cson_t_plus_dt = \
             Element1d.newton_raphson_for_ve(self.proprietes.material.eos,
@@ -180,25 +158,6 @@ class Element1d(Element):
     def calculer_nouvo_taille(self):
         """
         Calcul de la nouvelle longueur de l'élément
-
-        TEST UNITAIRE
-        >>> import numpy as np
-        >>> from xvof.node import Node1d
-        >>> from xvof.miscellaneous import *
-        >>> from xvof.equationsofstate import MieGruneisen
-        >>> ee = MieGruneisen()
-        >>> num_props = numerical_props(0.2, 1.0, 0.35)
-        >>> mat_props = material_props(1.0e+05, 0.0, 8129., ee)
-        >>> geom_props = geometrical_props(1.0e-06)
-        >>> props = properties(num_props, mat_props, geom_props)
-        >>> noda = Node1d(1, poz_init=np.array([-1.0e-03]))
-        >>> nodb = Node1d(2, poz_init=np.array([1.5e-03]))
-        >>> my_elem = Element1d(props, 123, [noda, nodb])
-        >>> noda._xtpdt = np.array([5.0e-03])
-        >>> nodb._xtpdt = np.array([6.0e-03])
-        >>> my_elem.calculer_nouvo_taille()
-        >>> print my_elem.taille_t_plus_dt
-        0.001
         """
         self._size_t_plus_dt = abs(self.noeuds[0].coordtpdt[0] - 
                                     self.noeuds[1].coordtpdt[0])
@@ -207,24 +166,6 @@ class Element1d(Element):
         """
         Calcul de la densité à l'instant t+dt basé sur
         la conservation de la masse
-
-        TEST UNITAIRE
-        >>> import numpy as np
-        >>> from xvof.node import Node1d
-        >>> from xvof.miscellaneous import *
-        >>> from xvof.equationsofstate import MieGruneisen
-        >>> ee = MieGruneisen()
-        >>> num_props = numerical_props(0.2, 1.0, 0.35)
-        >>> mat_props = material_props(1.0e+05, 0.0, 8129., ee)
-        >>> geom_props = geometrical_props(1.0e-06)
-        >>> props = properties(num_props, mat_props, geom_props)
-        >>> noda = Node1d(1, poz_init=np.array([-1.0e-03]))
-        >>> nodb = Node1d(2, poz_init=np.array([1.5e-03]))
-        >>> my_elem = Element1d(props, 123, [nodb, noda])
-        >>> my_elem._size_t_plus_dt = 1.25e-03
-        >>> my_elem.calculer_nouvo_densite()
-        >>> print my_elem.rho_t_plus_dt
-        16258.0
         """
         self._rho_t_plus_dt = \
             self.rho_t * self.taille_t / self.taille_t_plus_dt
@@ -232,27 +173,6 @@ class Element1d(Element):
     def calculer_nouvo_pseudo(self, delta_t):
         """
         Calcul de la nouvelle pseudo
-
-        TEST UNITAIRE
-        >>> import numpy as np
-        >>> from xvof.node import Node1d
-        >>> from xvof.miscellaneous import *
-        >>> from xvof.equationsofstate import MieGruneisen
-        >>> ee = MieGruneisen()
-        >>> num_props = numerical_props(0.2, 1.0, 0.35)
-        >>> mat_props = material_props(1.0e+05, 0.0, 8129., ee)
-        >>> geom_props = geometrical_props(1.0e-06)
-        >>> props = properties(num_props, mat_props, geom_props)
-        >>> noe1 = Node1d(1, poz_init=np.array([4.0e-03]))
-        >>> noe2 = Node1d(2, poz_init=np.array([7.0e-03]))
-        >>> noe1._xtpdt = np.array([5.0e-03])
-        >>> noe2._xtpdt = np.array([6.0e-03])
-        >>> my_elem = Element1d(props, 123, [noe2, noe1])
-        >>> my_elem.calculer_nouvo_taille()
-        >>> my_elem.calculer_nouvo_densite()
-        >>> my_elem.calculer_nouvo_pseudo(1.0e-6)
-        >>> print my_elem.pseudo
-        2438700000.0
         """
         self._pseudo_plus_un_demi = Element1d.calculer_pseudo(delta_t,
             self.rho_t, self.rho_t_plus_dt, self.taille_t_plus_dt,
@@ -262,29 +182,6 @@ class Element1d(Element):
     def calculer_nouvo_dt(self):
         """
         Calcul du pas de temps dans l'élément
-
-        TEST UNITAIRE
-        >>> import numpy as np
-        >>> from xvof.node import Node1d
-        >>> from xvof.miscellaneous import *
-        >>> from xvof.equationsofstate import MieGruneisen
-        >>> ee = MieGruneisen()
-        >>> num_props = numerical_props(0.2, 1.0, 0.35)
-        >>> mat_props = material_props(1.0e+05, 0.0, 8129., ee)
-        >>> geom_props = geometrical_props(1.0e-06)
-        >>> props = properties(num_props, mat_props, geom_props)
-        >>> noe1 = Node1d(1, poz_init=np.array([4.0e-03]))
-        >>> noe2 = Node1d(2, poz_init=np.array([7.0e-03]))
-        >>> noe1._x_t_plus_dt = np.array([5.0e-03])
-        >>> noe2._x_t_plus_dt = np.array([7.0e-03])
-        >>> my_elem = Element1d(props, 123, [noe1, noe2])
-        >>> my_elem.calculer_nouvo_taille()
-        >>> my_elem.calculer_nouvo_densite()
-        >>> my_elem.calculer_nouvo_pseudo(1.0e-6)
-        >>> my_elem.calculer_nouvo_pression()
-        >>> my_elem.calculer_nouvo_dt()
-        >>> print my_elem.delta_t
-        2.63819095477e-07
         """
         cfl = self.proprietes.numeric.cfl
         self._dt = Element1d.calculer_dt(cfl, self.rho_t, self.rho_t_plus_dt,
@@ -294,42 +191,38 @@ class Element1d(Element):
 #######          PROGRAMME PRINCIPAL        ###############
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 if __name__ == "__main__":
-    import doctest
-    TESTRES = doctest.testmod(verbose=0)
-    if(TESTRES[0] == 0):
-        print "TESTS UNITAIRES : OK"
-        #
-        # Lancemet du profiling
-        #
-        from xvof.utilities import timeit_file
-        from os import system
-        from xvof.miscellaneous import numerical_props, material_props
-        from xvof.miscellaneous import geometrical_props, properties
-        from xvof.equationsofstate import MieGruneisen
-        from xvof.node import Node1d
-        #
+    #
+    # Lancemet du profiling
+    #
+    from xvof.utilities import timeit_file
+    from os import system
+    from xvof.miscellaneous import numerical_props, material_props
+    from xvof.miscellaneous import geometrical_props, properties
+    from xvof.equationsofstate import MieGruneisen
+    from xvof.node import Node1d
+    #
 
-        @timeit_file('calculer_nouvo_pression.log')
-        def profil_calculer_nouvo_pression(element, it_nb=100):
-            """
-            Fait it_nb appel(s) à calculer_nouvo_pression à
-            des fins de profiling
-            """
-            i = 0
-            while i < it_nb:
-                element.calculer_nouvo_pression()
-                i += 1
-        #
-        EE = MieGruneisen()
-        NUM_PROPS = numerical_props(0.2, 1.0, 0.35)
-        MAT_PROPS = material_props(1.0e+05, 0.0, 8129., EE)
-        GEOM_PROPS = geometrical_props(1.0e-06)
-        PROPS = properties(NUM_PROPS, MAT_PROPS, GEOM_PROPS)
-        NODA = Node1d(1, np.array([0.5e-03]))
-        NODB = Node1d(2, np.array([3.0e-03]))
-        MY_ELEM = Element1d(PROPS, 123, [NODA, NODB])
-        MY_ELEM._rho_t_plus_dt = 9000.0
-        NBR_ITER = 100000
-        print "Lancement profiling sur {:6d} itérations".format(NBR_ITER)
-        profil_calculer_nouvo_pression(MY_ELEM, NBR_ITER)
-        system('cat calculer_nouvo_pression.log')
+    @timeit_file('calculer_nouvo_pression.log')
+    def profil_calculer_nouvo_pression(element, it_nb=100):
+        """
+        Fait it_nb appel(s) à calculer_nouvo_pression à
+        des fins de profiling
+        """
+        i = 0
+        while i < it_nb:
+            element.calculer_nouvo_pression()
+            i += 1
+    #
+    EE = MieGruneisen()
+    NUM_PROPS = numerical_props(0.2, 1.0, 0.35)
+    MAT_PROPS = material_props(1.0e+05, 0.0, 8129., EE)
+    GEOM_PROPS = geometrical_props(1.0e-06)
+    PROPS = properties(NUM_PROPS, MAT_PROPS, GEOM_PROPS)
+    NODA = Node1d(1, np.array([0.5e-03]))
+    NODB = Node1d(2, np.array([3.0e-03]))
+    MY_ELEM = Element1d(PROPS, 123, [NODA, NODB])
+    MY_ELEM._rho_t_plus_dt = 9000.0
+    NBR_ITER = 100000
+    print "Lancement profiling sur {:6d} itérations".format(NBR_ITER)
+    profil_calculer_nouvo_pression(MY_ELEM, NBR_ITER)
+    system('cat calculer_nouvo_pression.log')
