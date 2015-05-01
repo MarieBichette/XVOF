08fd4351f0f1861357b38b331e8476f6cd52a8ef
hippo91 <guillaume.peillex@gmail.com>
2015-05-01 15:17:26 +0200

    Des bricoles mais surtout prise en charge du pas de temps critique

--- xvof/launcher.py
+++ xvof/launcher.py
@@ -13,13 +13,13 @@ if __name__ == '__main__':
     print "Création des propriétés"
     equation_detat = MieGruneisen()
     num_props = numerical_props(0.2, 1.0, 0.35)
-    mat_props = material_props(100019.216, 7.69, 8129., equation_detat)
+    mat_props = material_props(100149.28, 7.7, 8129., equation_detat)
     geom_props = geometrical_props(1.0e-06)
     props = properties(num_props, mat_props, geom_props)
     print "=>OK"
-    tfinal = 15.0e-06
+    tfinal = 150.0e-06
     time = 0.
-    step = 0.
+    step = 0
     dt = 4.0e-09
     pcharg = 0.5e+09
     coord_init = np.linspace(0, 1.0, 101)
@@ -30,17 +30,14 @@ if __name__ == '__main__':
     print "Calcul de la masse des noeuds :"
     my_mesh.calculer_masse_des_noeuds()
     print "=> OK"
-    print "AFFICHAGE DES CHAMPS A L'INIT"
-    print my_mesh.pressure_t_field
-    print my_mesh.pressure_t_plus_dt_field
-    raw_input("Lancer le calcul?")
     print "LANCEMENT DU CALCUL!"
+    dt_crit = 1e+07
     while (time < tfinal):
-        print "Calcul du temps {:15.9g} secondes avec un pas de temps de {:15.9g} secondes"\
-            .format(time, dt)
-        print "Application du chargement :"
-        my_mesh.nodes[0]._force[:] = pcharg * geom_props.section
-        print "=>OK"
+        if(dt_crit < dt):
+            raise SystemExit("Le pas de temps critique est plus petit que le pas de temps")
+        print "Itération N°{:<4d} -- Calcul du temps {:15.9g} secondes avec un pas de temps de {:15.9g} secondes"\
+            .format(step, time, dt)
+        print "   <- Pas de temps critique = {:15.9g} ->".format(dt_crit)
         print "Calcul de la nouvelle vitesse des noeuds :"
         my_mesh.calculer_nouvo_vit_noeuds(dt)
         # print my_mesh.velocity_t_plus_half_field
@@ -48,7 +45,8 @@ if __name__ == '__main__':
         print "Calcul des nouvelles coordonnées des noeuds :"
         my_mesh.calculer_nouvo_coord_noeuds(dt)
         # print my_mesh.coord_t_plus_dt_field
-        print "Calcul des nouvelles tailes des éléments :"
+        print "=>OK"        
+        print "Calcul des nouvelles tailles des éléments :"
         my_mesh.calculer_nouvo_taille_des_elements()
         # print my_mesh.size_t_plus_dt_field
         print "=> OK"
@@ -62,10 +60,15 @@ if __name__ == '__main__':
         print "=> OK"
         print "Calcul des nouvelles forces nodales :"
         my_mesh.calculer_nouvo_force_des_noeuds()
+        # print "Application du chargement :"
+        my_mesh.nodes[0]._force[:] += pcharg * geom_props.section
+        my_mesh.nodes[-1]._force[:] += -100149.28 * geom_props.section
         # print my_mesh.force_field
         print "=>OK"
         print "Incrémentation"
+        dt_crit = my_mesh.calculer_nouvo_pdt_critique()
         my_mesh.incrementer()
+        dt = 0.35 * dt_crit
         time += dt
         step += 1
         # raw_input("Next step?")
