7e257ec5d0eb0907ddc584694c5bdb029ba989fd
hippo91 <guillaume.peillex@gmail.com>
2015-05-03 14:12:18 +0200

    Un peu de réorganisation + commentaires Mise en commentaires du rejet en cas de pas de temps critique inférieur au pas de temps

--- xvof/VNR_1D_ClassicalElement.py
+++ xvof/VNR_1D_ClassicalElement.py
@@ -3,56 +3,59 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-
 from xvof.equationsofstate.miegruneisen import MieGruneisen
 from xvof.figure_manager.figure_manager import FigureManager
-
 from xvof.mesh.mesh1d import Mesh1d
 from xvof.miscellaneous import geometrical_props, material_props
 from xvof.miscellaneous import numerical_props, properties
+
 
 #  =================================================
 #  = PARAMETRES DE LA SIMULATION                   =
 TFinal = 15.0e-06
 DT_init = 4.0e-09
+DT_max = 4.0e-09
 PChargement = 5.0e+09
-Longueur = 0.1
-NbrElements = 300
+Longueur = 10.0e-03
+NbrElements = 100
 CFL = 0.35
 
-NbrImages = 25
+NbrImages = 0
 #  =================================================
 
 if __name__ == '__main__':
-    # ---------------------------------------------#
-    #         CREATION DE L'ENVIRONNEMENT          #
-    # ---------------------------------------------#
-    print "Création des propriétés"
-    equation_detat = MieGruneisen()
-    num_props = numerical_props(0.2, 1.0, 0.35)
-    mat_props = material_props(100149.28, 7.7, 8129., equation_detat)
-    geom_props = geometrical_props(1.0e-06)
-    props = properties(num_props, mat_props, geom_props)
-    print "=>OK"
-    print "Création du maillage"
+    #
     tfinal = TFinal
     time = 0.
     step = 0
     dt = DT_init
     dt_crit = 2 * dt
     pcharg = PChargement
+    # ---------------------------------------------#
+    #         CREATION DES PROPRIETES              #
+    # ---------------------------------------------#
+    equation_detat = MieGruneisen()
+    num_props = numerical_props(0.2, 1.0, 0.35)
+    mat_props = material_props(100149.28, 7.7, 8129., equation_detat)
+    geom_props = geometrical_props(1.0e-06)
+    props = properties(num_props, mat_props, geom_props)
+    # ---------------------------------------------#
+    #         CREATION DU MAILLAGE                 #
+    # ---------------------------------------------#
     coord_init = np.linspace(0, Longueur, NbrElements + 1)
     vit_init = np.zeros(NbrElements + 1)
     my_mesh = Mesh1d(props, initial_coordinates=coord_init,
                      initial_velocities=vit_init)
-    my_fig_manager = FigureManager(my_mesh)
-    #
-    delta_t_images = tfinal / NbrImages
+    # ---------------------------------------------#
+    #  MISE EN PLACE DU GESTIONNAIRE DE FIGURES    #
+    # ---------------------------------------------#
+    if (NbrImages != 0):
+        delta_t_images = tfinal / NbrImages
+        my_fig_manager = FigureManager(my_mesh)
+        my_fig_manager.populate_figs()
+    else:
+        delta_t_images = tfinal * 2.0
     t_next_image = delta_t_images
-    print "=> OK"
-    print "Initialisation des figures"
-    my_fig_manager.populate_figs()
-    print "=>OK"
     # ---------------------------------------------#
     #         CALCUL DES MASSES NODALES            #
     # ---------------------------------------------#
@@ -61,8 +64,8 @@ if __name__ == '__main__':
     print "=> OK"
     print "LANCEMENT DU CALCUL!"
     while (time < tfinal):
-        if(dt_crit < dt):
-            raise SystemExit("Le pas de temps critique est plus petit que le pas de temps")
+#         if(dt_crit < dt):
+#             raise SystemExit("Le pas de temps critique est plus petit que le pas de temps")
         print "Itération N°{:<4d} -- Calcul du temps {:15.9g} secondes avec un pas de temps de {:15.9g} secondes"\
             .format(step, time, dt)
         print "   <- Pas de temps critique = {:15.9g} ->".format(dt_crit)
@@ -107,7 +110,7 @@ if __name__ == '__main__':
         #                INCREMENTATION                #
         # ---------------------------------------------#
         my_mesh.incrementer()
-        dt = CFL * dt_crit
+        # dt = min([dt, CFL * dt_crit])
         time += dt
         step += 1
         # ---------------------------------------------#
@@ -118,5 +121,5 @@ if __name__ == '__main__':
             my_fig_manager.update_figs()
             t_next_image += delta_t_images
             print "=>OK"
-
+  
     plt.show()
