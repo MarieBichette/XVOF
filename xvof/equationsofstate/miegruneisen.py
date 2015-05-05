5f3d1e5e2a3ddc3618fedc49083aac79f0fb0fe9
hippo91 <guillaume.peillex@gmail.com>
2015-05-05 08:34:44 +0200

    Suppression du main -- Module only

--- xvof/equationsofstate/miegruneisen.py
+++ xvof/equationsofstate/miegruneisen.py
@@ -213,32 +213,3 @@ class MieGruneisen(EquationOfState):
 
     def solve_vt(self, specific_volume, temperatue):
         raise NotImplementedError
-
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-############ PROGRAMME PRINCIPAL ####################
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\
-if __name__ == '__main__':
-    #
-    # Lancemet du profiling
-    #
-    from xvof.utilities import timeit_file
-    from os import system
-    #
-
-    @timeit_file('solve_ve.log')
-    def profil_solve_ve(equation_of_state, spec_vol, e_int, it_nb=100):
-        """
-        Fait it_nb appel(s) à solve_ve à des fins de profiling
-        """
-        i = 0
-        while i < it_nb:
-            equation_of_state.solve_ve(spec_vol, e_int)
-            i += 1
-    #
-    MY_EE = MieGruneisen()
-    RHO = 9.000001000003e+03
-    E_INT = 2.0e+03
-    NBR_ITER = 100000
-    print "Lancement profiling sur {:6d} itérations".format(NBR_ITER)
-    profil_solve_ve(MY_EE, 1. / RHO, E_INT, NBR_ITER)
-    system('cat solve_ve.log')
