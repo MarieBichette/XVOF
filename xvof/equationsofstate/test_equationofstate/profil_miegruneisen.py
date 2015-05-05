0318c0513888632ec2b4651af7a9e27325c7f6d7
hippo91 <guillaume.peillex@gmail.com>
2015-05-05 08:35:27 +0200

    Ajout du fichier de profiling de MieGruneisen.solve_ve

---
+++ xvof/equationsofstate/test_equationofstate/profil_miegruneisen.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2.7
+# -*- coding: iso-8859-1 -*-
+
+if __name__ == '__main__':
+    #
+    # Lancement du profiling
+    #
+    from xvof.utilities import timeit_file
+    from os import system
+    from random import random
+    from xvof.equationsofstate.miegruneisen import MieGruneisen
+    #
+
+    @timeit_file('/tmp/solve_ve.log')
+    def profil_solve_ve(equation_of_state, spec_vol, e_int, it_nb=100):
+        """
+        Fait it_nb appel(s) à solve_ve à des fins de profiling
+        """
+        i = 0
+        while i < it_nb:
+            equation_of_state.solve_ve(spec_vol, e_int)
+            i += 1
+    #
+    MY_EE = MieGruneisen()
+    RHO = 8000.0 + random() * 1000.0
+    E_INT = random() * 2.0e+03
+    NBR_ITER = 1110000
+    print "Lancement profiling sur {:6d} itérations".format(NBR_ITER)
+    profil_solve_ve(MY_EE, 1. / RHO, E_INT, NBR_ITER)
+    system('cat /tmp/solve_ve.log')
