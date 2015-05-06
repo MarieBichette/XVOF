181085aea913aecab4e3659aded3dc4460c6efc0
hippo91 <guillaume.peillex@gmail.com>
2015-05-06 14:36:17 +0200

    Ajout de la possibilité d'annoter le titre + enregistrement

--- xvof/figure_manager/physic_figure.py
+++ xvof/figure_manager/physic_figure.py
@@ -3,6 +3,8 @@
 """
 Classe définissant une figure
 """
+from os import sep
+
 import matplotlib.pyplot as plt
 
 
@@ -10,14 +12,16 @@ class PhysicFigure(object):
     """
     Figure
     """
-    def __init__(self, X, Y, xlabel="X", ylabel="Y", titre="titre"):
+    def __init__(self, X, Y, xlabel="X", ylabel="Y", titre="titre", save_path=None):
         self._fig = plt.figure()
         self._ax = self._fig.add_subplot(111)
         self._line, = self._ax.plot(X, Y, '-')
         self._ax.set_xlabel(xlabel)
         self._ax.set_ylabel(ylabel)
         self._ax.set_title(titre)
-        plt.show(block=False)
+        self._save_path = save_path
+        self._fig_number = 1
+        self._title = titre
 
     def set_y_limit(self, val_min=0., val_max=1.0):
         """ Fixation des limites en y"""
@@ -27,11 +31,21 @@ class PhysicFigure(object):
         """ Fixation des limites en x"""
         self._ax.set_xlim([val_min, val_max])
 
-    def update(self, X=None, Y=None):
-        """ Mise à jour de l'image pour avoir une animation"""
+    def update(self, X=None, Y=None, title_comp=None):
+        """
+        Mise à jour de l'image pour avoir une animation
+        Sauvegarde de l'image si présence d'un path
+        """
         if(X is not None):
             self._line.set_xdata(X)
         if(Y is not None):
             self._line.set_ydata(Y)
+        if(title_comp is not None):
+            self._ax.set_title(self._title + ' ' + title_comp)
         self._fig.canvas.draw()
-        plt.show(block=False)
+        if (self._save_path is not None):
+            fig_path = self._save_path + sep + self._title
+            fig_path += "_{:04d}.png".format(self._fig_number)
+            fig_path = fig_path.replace(" ", "_")
+            self._fig.savefig(fig_path)
+            self._fig_number += 1
