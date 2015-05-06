e326ef33d5075a9f7417a377a2ee4b1bf0799a4b
hippo91 <guillaume.peillex@gmail.com>
2015-05-06 14:34:53 +0200

    Ajout de la possibilité d'enregistrer les images et/ou de les afficher

--- xvof/figure_manager/figure_manager.py
+++ xvof/figure_manager/figure_manager.py
@@ -7,28 +7,33 @@ Classe dÃ©finissant le gestionnaire d'images
 @todo: Transformer la classe Field en NamedTuple (cf. properties)
 """
 from collections import namedtuple
+from os import makedirs
+from os.path import exists
 
+import matplotlib.pyplot as plt
 import numpy as np
 from physic_figure import PhysicFigure
 
 
-Field = namedtuple("Field", ["label", "titre", "val_min", "val_max"])
+Field = namedtuple("Field", ["label", "titre", "val_min", "val_max", "results_path"])
 
-PressureField = Field("Pression [Pa]", "Champ de pression", 0, 7.5e+09)
-DensityField = Field("Masse volumique [kg/m3]", "Champ de densite", 8000.0, 8500.0)
-InternalEnergyField = Field("Energie interne [J/kg]", "Champ d energie interne", 0, 40000.0)
-PseudoViscosityField = Field("Pseudoviscosite [Pa]", "Champ de pseudoviscosite", 0, 1.0e+09)
-CellPositionField = Field("Position [m]", "Champ de position", 0, 0.02)
+PressureField = Field("Pression [Pa]", "Champ de pression", 0, 7.5e+09, "./RESULTATS/PressureField")
+DensityField = Field("Masse volumique [kg/m3]", "Champ de densite", 8000.0, 8500.0, "./RESULTATS/DensityField")
+InternalEnergyField = Field("Energie interne [J/kg]", "Champ d energie interne", 0, 40000.0, "./RESULTATS/InternalEnergyField")
+PseudoViscosityField = Field("Pseudoviscosite [Pa]", "Champ de pseudoviscosite", 0, 1.0e+09, "./RESULTATS/PseudoViscosityField")
+CellPositionField = Field("Position [m]", "Champ de position", 0, 0.02, "./RESULTATS/CellPositionField")
 
 class FigureManager(object):
     """
     Gestionnaire de figures
     """
-    def __init__(self, mesh_instance):
+    def __init__(self, mesh_instance, dump=False, show=True):
         self.__mesh_instance = mesh_instance
         self.__figures_mailles = []
         self.__champs_mailles = {}
         self.update_fields()
+        self._dump = dump
+        self._show = show
 
     def update_fields(self):
         """ MAJ des champs par appel des propriétés du maillage"""
@@ -55,7 +60,11 @@ class FigureManager(object):
         except ValueError as ve:
             print "Le champ {} est inconnu!".format(field_Y)
             raise ve
-        phyfig = PhysicFigure(X, Y, xlabel=field_X.label, ylabel=field_Y.label,
+        if self._dump:
+            phyfig = PhysicFigure(X, Y, xlabel=field_X.label, ylabel=field_Y.label,
+                              titre=field_Y.titre, save_path=field_Y.results_path)
+        else:
+            phyfig = PhysicFigure(X, Y, xlabel=field_X.label, ylabel=field_Y.label,
                               titre=field_Y.titre)
         phyfig.set_y_limit(field_Y.val_min, field_Y.val_max)
         phyfig.set_x_limit(field_X.val_min, field_X.val_max)
@@ -71,8 +80,12 @@ class FigureManager(object):
             if champ_Y != champ_X:
                 fig = self.create_figure_for_cell_field(champ_X, champ_Y)
                 self.__figures_mailles.append((fig, champ_X, champ_Y))
+        if self._show:
+            plt.show(block=False)
+        if self._dump:
+            self.create_reps()
 
-    def update_figs(self):
+    def update_figs(self, title_compl=None):
         """
         MAJ des champs puis des figures correspondantes
         """
@@ -80,4 +93,19 @@ class FigureManager(object):
         for (fig, champ_x, champ_y) in self.__figures_mailles:
             champ_x_valeurs = self.__champs_mailles[champ_x]
             champ_y_valeurs = self.__champs_mailles[champ_y]
-            fig.update(champ_x_valeurs, champ_y_valeurs)
+            fig.update(champ_x_valeurs, champ_y_valeurs, title_compl)
+        if self._show:
+            plt.show(block=False)
+
+    def create_reps(self):
+        """
+        Création des répertoires où sont stockées les figures
+        """
+        for (_, _, field) in self.__figures_mailles:
+            path = field.results_path
+            if (exists(path)):
+                msg = "Le chemin {:s} existe déjà!"
+                msg += "\nAbandon pour éviter d'écraser des données!"
+                raise SystemExit(msg)
+            else:
+                makedirs(path)
