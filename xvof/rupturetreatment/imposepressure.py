1e8953919b2b157bb296ecfb1aaf6401d4d566bc
hippo91 <guillaume.peillex@gmail.com>
2015-05-15 18:15:06 +0200

    Changement de commentaires

--- xvof/rupturetreatment/imposepressure.py
+++ xvof/rupturetreatment/imposepressure.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2.7
 # -*- coding: iso-8859-1 -*-
 """
-Classe définissant un critère de rupture basé sur la pression minimum
+Classe définissant un traitement de la rupture par imposition de la pression
 """
 from xvof.rupturetreatment.rupturetreatment import RuptureTreatment
 
