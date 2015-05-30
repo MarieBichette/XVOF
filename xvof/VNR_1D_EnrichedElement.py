49a52839ac22ad24b74faf6682c7b652845177af
hippo91 <guillaume.peillex@gmail.com>
2015-05-30 20:43:18 +0200

    Chgt affichage des infos + paramètres calcul

--- xvof/VNR_1D_EnrichedElement.py
+++ xvof/VNR_1D_EnrichedElement.py
@@ -20,19 +20,26 @@ def print_infos_about_enrichment(mesh, titre="", cells=None, nodes=None):
     has_been_enriched = False
     print "<--- {} --->".format(titre)
     if cells is not None:
+        print " Nombre de mailles : {:d}".format(len(cells))
         for cell in cells:
             if isinstance(cell, Element1dUpgraded):
                 indice = cell.indice
-                cell_g = mesh.cells[indice-1]
-                cell_d = mesh.cells[indice+1]
+                cell_g = mesh.cells[indice - 1]
+                cell_d = mesh.cells[indice + 1]
                 cell_g.infos()
                 cell.infos()
                 cell_d.infos()
                 has_been_enriched = True
     if nodes is not None:
+        print " Nombre de noeuds : {:d}".format(len(nodes))
         for node in nodes:
             if isinstance(node, Node1dUpgraded):
+                index = node.index
+                node_g = mesh.nodes[index - 1]
+                node_d = mesh.nodes[index + 1]
+                node_g.infos()
                 node.infos()
+                node_d.infos()
                 has_been_enriched = True
     if has_been_enriched:
         raw_input("Poursuivre?")
@@ -41,14 +48,14 @@ def print_infos_about_enrichment(mesh, titre="", cells=None, nodes=None):
 #  =================================================
 #  = PARAMETRES DE LA SIMULATION                   =
 TempsFinal = 15.0e-06
-PasDeTempsInit = 4.0e-09
+PasDeTempsInit = 1.0e-09
 PressionInit = 100149.28
 EnergieInterneInit = 7.7
 RhoInit = 8129.
 EquationEtat = MieGruneisen()
 PChargementGauche = ConstantPressure(-3.5e+09)
-#PChargementGauche = TwoStepsPressure(15.0e+09, 0e+09, TempsFinal / 3.0)
-#PChargementDroite = ConstantPressure(PressionInit)
+# PChargementGauche = TwoStepsPressure(15.0e+09, 0e+09, TempsFinal / 3.0)
+# PChargementDroite = ConstantPressure(PressionInit)
 PChargementDroite = ConstantPressure(-3.5e+09)
 CritereRupture = MinimumPressureCriterion(-7.0e+09)
 TraitementRupture = EnrichElement(0.5)
@@ -58,7 +65,7 @@ ParamPseudoA = 0.2
 ParamPseudoB = 1.0
 CFL = 0.35
 
-NbrImages = 250
+NbrImages = 3750
 #  =================================================
 
 if __name__ == '__main__':
@@ -104,40 +111,43 @@ if __name__ == '__main__':
         print "Itération N°{:<4d} -- Calcul du temps {:15.9g} secondes avec un pas de temps de {:15.9g} secondes"\
             .format(step, time, dt)
         print "   <- Pas de temps critique = {:15.9g} ->".format(dt_crit)
+        print_infos_about_enrichment(my_mesh, titre="DEBUT DE CYCLE", cells=my_mesh.cells, nodes=my_mesh.nodes)
         # ---------------------------------------------#
         #         CALCUL DES VITESSES NODALES          #
         # ---------------------------------------------#
         my_mesh.calculer_nouvo_vit_noeuds(dt)
-        print_infos_about_enrichment(my_mesh, titre="VITESSES NODALES", nodes=my_mesh.nodes)
+#         print_infos_about_enrichment(my_mesh, titre="VITESSES NODALES", nodes=my_mesh.nodes)
         # ---------------------------------------------#
         #         CALCUL DES COORDONNEES NODALES       #
         # ---------------------------------------------#
         my_mesh.calculer_nouvo_coord_noeuds(dt)
-        print_infos_about_enrichment(my_mesh, titre="COORDONNEES NODALES", nodes=my_mesh.nodes)
+#         print_infos_about_enrichment(my_mesh, titre="COORDONNEES NODALES", nodes=my_mesh.nodes)
         # ---------------------------------------------#
         #         CALCUL DES VOLUMES DES MAILLES       #
         # ---------------------------------------------#
         my_mesh.calculer_nouvo_taille_des_elements(dt)
-        print_infos_about_enrichment(my_mesh, titre="VOLUMES DES MAILLES", cells=my_mesh.cells)
+#         print_infos_about_enrichment(my_mesh, titre="VOLUMES DES MAILLES", cells=my_mesh.cells)
         # ---------------------------------------------#
         #         CALCUL DES DENSITES DES MAILLES      #
         # ---------------------------------------------#
         my_mesh.calculer_nouvo_densite_des_elements()
+#         print_infos_about_enrichment(my_mesh, titre="DENSITE DES MAILLES", cells=my_mesh.cells)
         # ---------------------------------------------#
         #         CALCUL DES PRESSIONS                 #
         # ---------------------------------------------#
         my_mesh.calculer_nouvo_pression_des_elements()
+#         print_infos_about_enrichment(my_mesh, titre="PRESSION DES MAILLES", cells=my_mesh.cells)
         # ---------------------------------------------#
         #              RUPTURE                         #
         # ---------------------------------------------#
         my_mesh.get_ruptured_cells(CritereRupture)
         my_mesh.apply_rupture_treatment(TraitementRupture)
-        print_infos_about_enrichment(my_mesh, titre="APRES RUPTURE", cells=my_mesh.cells, nodes=my_mesh.nodes)
+#         print_infos_about_enrichment(my_mesh, titre="APRES RUPTURE", cells=my_mesh.cells, nodes=my_mesh.nodes)
         # ---------------------------------------------#
         #         CALCUL DES FORCES NODALES            #
         # ---------------------------------------------#
         my_mesh.calculer_nouvo_force_des_noeuds()
-        print_infos_about_enrichment(my_mesh, titre="FORCES NODALES", nodes=my_mesh.nodes)
+#         print_infos_about_enrichment(my_mesh, titre="FORCES NODALES", nodes=my_mesh.nodes)
         # ---------------------------------------------#
         #         APPLICATION DU CHARGEMENT            #
         # ---------------------------------------------#
@@ -155,6 +165,7 @@ if __name__ == '__main__':
         #                INCREMENTATION                #
         # ---------------------------------------------#
         my_mesh.incrementer()
+#         print_infos_about_enrichment(my_mesh, titre="INCREMENTATION", cells=my_mesh.cells, nodes=my_mesh.nodes)
 #         dt = min([dt, num_props.cfl * dt_crit])
         time += dt
         step += 1
@@ -163,7 +174,8 @@ if __name__ == '__main__':
         # ---------------------------------------------#
         if (time > t_next_image):
             print "Affichage des images"
-            my_fig_manager.update_figs("t={:5.4g} us".format(time / 1.e-06))
+            if (time > 1.4e-06):
+                my_fig_manager.update_figs("t={:5.4g} us".format(time / 1.e-06))
             t_next_image += delta_t_images
             print "=>OK"
 
