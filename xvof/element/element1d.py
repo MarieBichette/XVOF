a2f4c19fe2da3e9a63be2c1e66376a0346c115e2
hippo91 <guillaume.peillex@gmail.com>
2015-07-20 21:59:19 +0200

    Suppression des méthodes de classes pour le calcul de la fonction à annuler et la résolution du Newton Raphson. Appel des émthodes du solveur qui les remplacent

--- xvof/element/element1d.py
+++ xvof/element/element1d.py
@@ -6,8 +6,9 @@ Classe définissant un élément en 1d
 # --------------------------------------------------------
 #               IMPORTATIONS DIVERSES                    #
 # --------------------------------------------------------
-import numpy as np
 from xvof.element import Element
+from xvof.solver.newtonraphson import NewtonRaphson
+from xvof.solver.functionstosolve.vnrenergyevolutionforveformulation import VnrEnergyEvolutionForVolumeEnergyFormulation
 
 
 # --------------------------------------------------------
@@ -20,74 +21,13 @@ class Element1d(Element):
     nbr_noeuds = 2
 
     @classmethod
-    def computeFunctionAndDerivative(cls, eos, enerj, rho_new, rho_old, pression_t, enerj_old):
-        """
-        Fonction à annuler et sa dérivée pour le schéma VNR
-        Formulation v-e
-        """
-        (p_i, dpsurde, dummy) = eos.solveVolumeEnergy(1. / rho_new, enerj)
-        # Fonction à annuler
-        delta_v = 1. / rho_new - 1. / rho_old
-        func = enerj + p_i * delta_v / 2. + pression_t * delta_v / 2. - \
-            enerj_old
-        # Dérivée de la fonction à annuler
-        dfunc = 1 + dpsurde * delta_v / 2.
-        return (func, dfunc)
-
-    @classmethod
-    def executeNewtonRaphsonForVolumeEnergyFormulation(cls, function, eos, old_state, rho_new):
-        """
-        Algorithme de Newton-Raphson pour déterminer le couple
-        energie/pression au pas de temps suivant
-        Formulation v-e
-        """
-        rho_old = old_state['Density']
-        pression_old = old_state['Pressure']
-        pseudo_old = old_state['Pseudo']
-        nrj_old = old_state['Energy']
-        iter_max = 20
-        pression_t = pression_old + 2. * pseudo_old
-        # Variable du Newton
-        nrj_i = nrj_old
-        # Critère de convergence
-        convergence = False
-        # Nombre d'itérations
-        nit = 0
-        #
-        while not convergence and (nit < iter_max):
-            (func_i, dfunc_i_surde) = \
-                function(eos, nrj_i, rho_new, rho_old, pression_t, nrj_old)
-            # Correction
-            nrj_iplus1 = nrj_i - func_i / dfunc_i_surde
-            nit += 1
-            if abs(func_i) < 1e-09:
-                convergence = True
-                res_nrj = nrj_i
-                res_pression_t_plus_dt, dummy, res_cson = \
-                    eos.solveVolumeEnergy(1. / rho_new, res_nrj)
-                break
-            if abs(dfunc_i_surde) < 1.e-09:
-                print "Sortie du NR par manque de pente :-)"
-                convergence = True
-                res_nrj = nrj_i
-                res_pression_t_plus_dt, dummy, res_cson = \
-                    eos.solveVolumeEnergy(1. / rho_new, res_nrj)
-                break
-            # Incrémentation
-            nrj_i = nrj_iplus1
-        if nit == iter_max:
-            print "Erreur de convergence du NR"
-            print "func_i=", func_i
-            print "nit=", nit
-            raise ValueError()
-        return res_nrj, res_pression_t_plus_dt, res_cson
-
-    @classmethod
     def computePseudo(cls, delta_t, rho_old, rho_new, size_new,
                       cel_son, a_pseudo, b_pseudo):
         """
         Calcul de la pseudo
         """
+        # pylint: disable=too-many-arguments
+        # 8 arguments semblent nécessaires...
         vnt = 1. / rho_old
         vnplusun = 1. / rho_new
         vnplusundemi = 0.5 * (vnt + vnplusun)
@@ -119,6 +59,8 @@ class Element1d(Element):
 
     def __init__(self, proprietes):
         Element.__init__(self, proprietes)
+        self._function_to_vanish = VnrEnergyEvolutionForVolumeEnergyFormulation()
+        self._solver = NewtonRaphson(self._function_to_vanish, self.nrj_t)
 
     # --------------------------------------------------------
     #            DEFINITION DES PROPRIETES                   #
@@ -139,15 +81,16 @@ class Element1d(Element):
         Formulation v-e
         """
         try:
-            eos = self.proprietes.material.eos
-            function_to_vanish = Element1d.computeFunctionAndDerivative
-            old_state = {'Pressure': self.pression_t,
-                         'Density': self.rho_t,
-                         'Pseudo': self.pseudo,
-                         'Energy': self.nrj_t}
-            self._nrj_t_plus_dt, self._pression_t_plus_dt, self._cson_t_plus_dt = \
-                Element1d.executeNewtonRaphsonForVolumeEnergyFormulation(function_to_vanish, eos,
-                                                                         old_state, self.rho_t_plus_dt)
+            my_variables = {'EquationOfState': self.proprietes.material.eos,
+                            'OldDensity': self.rho_t,
+                            'NewDensity': self.rho_t_plus_dt,
+                            'Pressure': self.pression_t + 2. * self.pseudo,
+                            'OldEnergy': self.nrj_t}
+            self._function_to_vanish.setVariables(my_variables)
+            self._nrj_t_plus_dt = self._solver.computeSolution()
+            self._pression_t_plus_dt, _, self._cson_t_plus_dt = \
+                self.proprietes.material.eos.solveVolumeEnergy(1. / self.rho_t_plus_dt, self.nrj_t_plus_dt)
+            self._function_to_vanish.eraseVariables()
         except ValueError as err:
             print "Element concerné : {}".format(self)
             raise err
@@ -198,43 +141,3 @@ class Element1d(Element):
         On impose la pression à t+dt (par exemple pour endommagement)
         """
         self._pression_t_plus_dt = pression
-
-# --------------------------------------------------------
-#            PROGRAMME PRINCIPAL                         #
-# --------------------------------------------------------
-if __name__ == "__main__":
-    #
-    # Lancemet du profiling
-    #
-    from xvof.utilities import timeit_file
-    from os import system
-    from xvof.miscellaneous import numerical_props, material_props
-    from xvof.miscellaneous import geometrical_props, properties
-    from xvof.equationsofstate import MieGruneisen
-    from xvof.node import Node1d
-    #
-
-    @timeit_file('/tmp/calculer_nouvo_pression.log')
-    def profil_calculer_nouvo_pression(element, it_nb=100):
-        """
-        Fait it_nb appel(s) à calculer_nouvo_pression à
-        des fins de profiling
-        """
-        i = 0
-        while i < it_nb:
-            element.calculer_nouvo_pression()
-            i += 1
-    #
-    EE = MieGruneisen()
-    NUM_PROPS = numerical_props(0.2, 1.0, 0.35)
-    MAT_PROPS = material_props(1.0e+05, 0.0, 8129., EE)
-    GEOM_PROPS = geometrical_props(1.0e-06)
-    PROPS = properties(NUM_PROPS, MAT_PROPS, GEOM_PROPS)
-    NODA = Node1d(1, np.array([0.5e-03]))
-    NODB = Node1d(2, np.array([3.0e-03]))
-    MY_ELEM = Element1d(PROPS)
-    MY_ELEM._rho_t_plus_dt = 9000.0
-    NBR_ITER = 100000
-    print "Lancement profiling sur {:6d} itérations".format(NBR_ITER)
-    profil_calculer_nouvo_pression(MY_ELEM, NBR_ITER)
-    system('cat /tmp/calculer_nouvo_pression.log')
