2ec98cecbc0b5545cee029ff1ca796c64f8019e7
guillaume2 <guillaume.peillex@gmail.col>
2016-12-04 16:13:05 +0100

    Respect de la PEP8 ==> nom de méthode en lowercase Ajout d'un chronomètre pour mesurer le temps de calcul + affichage de celui-ci en fin de calcul

--- xvof/VNR_1D_EnrichedElement.py
+++ xvof/VNR_1D_EnrichedElement.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python2.7
 # -*- coding: iso-8859-15 -*-
 """
-:todo: merge avec VNR_1D_ClassicalElement
-:todo: retravailler perf de la lib externe de solver
-:todo: revoir calcul du pas de temps critique
+:todo: merge with VNR_1D_ClassicalElement
+:todo: increase perf of external solver library
+:todo: correct pb of variable time step
 """
 import matplotlib
 
@@ -16,10 +16,9 @@ from xvof.pressurelaw.twostepspressure import TwoStepsPressure
 from xvof.rupturecriterion.minimumpressure import MinimumPressureCriterion
 from xvof.data.data_container import DataContainer
 from xvof.mesh.mesh1denriched import Mesh1dEnriched
+import time
 
-#  =================================================
-#  SIMULATION PARAMETERS                           =
-#  =================================================
+# SIMULATION PARAMETERS
 data = DataContainer()
 # TIME MANAGEMENT
 FinalTime = data.time.final_time
@@ -28,13 +27,13 @@ InitialTimeStep = data.time.initial_time_step
 Length = data.geometric.length
 # MATTER
 InitialPressure = data.material.pression_init
-# NUMERIC
+#  NUMERIC
 NumberOfElements = data.numeric.cells_number
 # LOADING
 LeftBoundaryPressure = TwoStepsPressure(15e+09, InitialPressure, 2.0e-06)
 RightBoundaryPressure = ConstantPressure(InitialPressure)
 RuptureCriterion = MinimumPressureCriterion(-7.0e+09)
-RuptureTreatment = DataContainer().material.damage_treatment(DataContainer().material.damage_treatment_value)
+RuptureTreatment = data.material.damage_treatment(data.material.damage_treatment_value)
 # OUTPUT
 ImagesNumber = data.output.number_of_images
 #  =================================================
@@ -42,7 +41,7 @@ ImagesNumber = data.output.number_of_images
 if __name__ == '__main__':
     np.set_printoptions(formatter={'float': '{: 25.23g}'.format})
     #
-    time = 0.
+    simulation_time = 0.
     step = 0
     dt = InitialTimeStep
     dt_crit = 2 * dt
@@ -56,9 +55,9 @@ if __name__ == '__main__':
     # ---------------------------------------------#
     #  FIGURES MANAGER SETUP                       #
     # ---------------------------------------------#
-    if (ImagesNumber != 0):
+    my_fig_manager = FigureManager(my_mesh, dump=data.output.images_dump, show=data.output.images_show)
+    if ImagesNumber != 0:
         delta_t_images = FinalTime / ImagesNumber
-        my_fig_manager = FigureManager(my_mesh, dump=data.output.images_dump, show=data.output.images_show)
         my_fig_manager.populate_figs()
     else:
         delta_t_images = FinalTime * 2.0
@@ -66,68 +65,72 @@ if __name__ == '__main__':
     # ---------------------------------------------#
     #         NODAL MASS COMPUTATION               #
     # ---------------------------------------------#
-    my_mesh.computeCellsSizes()
-    my_mesh.computeNodesMasses()
+    my_mesh.compute_cells_sizes()
+    my_mesh.compute_nodes_masses()
     print "CALCULUS LAUNCHED!"
-    while time < FinalTime:
+    compute_time = 0.
+    while simulation_time < FinalTime:
+        loop_begin_time = time.time()
         if step % 1000 == 0:
             msg = ("""Iteration {:<4d} -- Time : {:15.9g} seconds with"""
-                   """ a time step of {:15.9g} seconds\n""").format(step, time, dt)
+                   """ a time step of {:15.9g} seconds\n""").format(step, simulation_time, dt)
             print msg
         # ---------------------------------------------#
         #         NODES VELOCITIES COMPUTATION         #
         # ---------------------------------------------#
-        my_mesh.computeNewNodesVelocities(dt)
+        my_mesh.compute_new_nodes_velocities(dt)
         # ---------------------------------------------#
         #         NODES COORDINATES COMPUTATION        #
         # ---------------------------------------------#
-        my_mesh.computeNewNodesCoordinates(dt)
+        my_mesh.compute_new_nodes_coordinates(dt)
         # ---------------------------------------------#
         #         CELLS VOLUMES COMPUTATION            #
         # ---------------------------------------------#
-        my_mesh.computeNewCellsSizes(dt)
+        my_mesh.compute_new_cells_sizes(dt)
         # ---------------------------------------------#
         #         CELLS DENSITIES COMPUTATION          #
         # ---------------------------------------------#
-        my_mesh.computeNewCellsDensities()
+        my_mesh.compute_new_cells_densities()
         # ---------------------------------------------#
         #         CELLS PRESSURES COMPUTATION          #
         # ---------------------------------------------#
-        my_mesh.computeNewCellsPressures()
+        my_mesh.compute_new_cells_pressures()
         # ---------------------------------------------#
         #              RUPTURE                         #
         # ---------------------------------------------#
-        my_mesh.getRupturedCells(RuptureCriterion)
-        my_mesh.applyRuptureTreatment(RuptureTreatment)
+        my_mesh.get_ruptured_cells(RuptureCriterion)
+        my_mesh.apply_rupture_treatment(RuptureTreatment)
         # ---------------------------------------------#
         #         NODES FORCES COMPUTATION             #
         # ---------------------------------------------#
-        my_mesh.computeNewNodesForces()
+        my_mesh.compute_new_nodes_forces()
         # ---------------------------------------------#
         #         LOADING                              #
         # ---------------------------------------------#
-        my_mesh.applyPressure('left', LeftBoundaryPressure.evaluate(time))
-        my_mesh.applyPressure('right', RightBoundaryPressure.evaluate(time))
+        my_mesh.apply_pressure('left', LeftBoundaryPressure.evaluate(simulation_time))
+        my_mesh.apply_pressure('right', RightBoundaryPressure.evaluate(simulation_time))
         # ---------------------------------------------#
         #         TIME STEP COMPUTATION                #
         # ---------------------------------------------#
-        dt_crit = my_mesh.computeNewTimeStep()
+        dt_crit = my_mesh.compute_new_time_step()
         # ---------------------------------------------#
         #         PSEUDOVISCOSITY COMPUTATION          #
         # ---------------------------------------------#
-        my_mesh.computeNewCellsPseudoViscosities(dt)
+        my_mesh.compute_new_cells_pseudo_viscosity(dt)
         # ---------------------------------------------#
         #                INCREMENTATION                #
         # ---------------------------------------------#
         my_mesh.increment()
-#         dt = dt_crit
-        time += dt
+        #         dt = dt_crit
+        simulation_time += dt
         step += 1
+        loop_end_time = time.time()
+        compute_time += loop_end_time - loop_begin_time
         # ---------------------------------------------#
         #                OUTPUT MANAGEMENT             #
         # ---------------------------------------------#
-        if (time > t_next_image):
-            my_fig_manager.update_figs("t={:5.4g} us".format(time / 1.e-06))
+        if simulation_time > t_next_image:
+            my_fig_manager.update_figs("t={:5.4g} us".format(simulation_time / 1.e-06))
             t_next_image += delta_t_images
-
+    print "Total time spent in compute operation is : {:15.9g} seconds".format(compute_time)
     plt.show()
