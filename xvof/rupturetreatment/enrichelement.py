3d2676accc6dd98d507e2a767b95a4df12e24282
hippo91 <guillaume.peillex@gmail.com>
2016-12-04 16:10:47 +0100

    Suppression de l'affectation de True à cells._enriched car cette variable membre à disparu (cf commit précédent) cells._classical ==> cells.classical

--- xvof/rupturetreatment/enrichelement.py
+++ xvof/rupturetreatment/enrichelement.py
@@ -26,7 +26,7 @@ class EnrichElement(RuptureTreatment):
         :param nodes: array of all nodes
         :param topology: topology of the problem
         """
-        if ruptured_cells.any() and not cells._enriched.any():  # Enrichment is made once for all
+        if ruptured_cells.any() and not cells.enriched.any():  # Enrichment is made once for all
             cells_to_be_enr = ruptured_cells
             # We keep only one cell to be enriched
             indices_cells_to_be_enr = np.where(cells_to_be_enr == True)
@@ -38,8 +38,7 @@ class EnrichElement(RuptureTreatment):
             nodes._classiques[nodes_to_be_enr] = False
             nodes.pos_disc = np.mean(nodes.xt[nodes_to_be_enr])
             print "==> Enrichement of cells : ", np.where(cells_to_be_enr == True)
-            cells._classical[cells_to_be_enr] = False
-            cells._enriched[cells_to_be_enr] = True
+            cells.classical[cells_to_be_enr] = False
             cells.right_size.new_value = (1. - self.__position_rupture) * cells.size_t_plus_dt
             cells.left_size.new_value = self.__position_rupture * cells.size_t_plus_dt
         ruptured_cells[:] = False
