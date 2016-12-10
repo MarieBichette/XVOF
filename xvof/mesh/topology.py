7fedc4855970561ede0c5a16ebb3b691978030fb
hippo91 <guillaume.peillex@gmail.com>
2016-12-10 16:51:45 +0100

    self.__nodes_belonging_to_cell et self.__cells_in_contact_with_node ne sont plus des listes. Ils sont None et c'est aux classes filles de les créer sous forme de tableau numpy. Suppression de méthodes non utilisées

--- xvof/mesh/topology.py
+++ xvof/mesh/topology.py
@@ -13,9 +13,9 @@ class Topology(object):
         #
         self._dim = dim
         # Array dont chaque item est un array des indices des noeuds appartenant à la maille
-        self._nodes_belonging_to_cell = [None for _ in xrange(nbr_of_cells)]
+        self._nodes_belonging_to_cell = None
         # Array dont chaque item est un array des indices des mailles en contact avec le noeud
-        self._cells_in_contact_with_node = [None for _ in xrange(nbr_of_nodes)]
+        self._cells_in_contact_with_node = None
 
     @property
     def dimension(self):
@@ -42,41 +42,15 @@ class Topology(object):
         '''
         self._nodes_belonging_to_cell[ind_cell] = np.array(ind_node_list)
 
-    def addNodeBelongingToCell(self, ind_cell, ind_node):
-        '''
-        Ajoute l'indice du noeud 'node' à la liste des indices des noeuds
-        appartenant à la maille d'indice 'ind_cell'
-
-        :param ind_cell: indice de la maille à laquelle attribuer l'indice du noeud
-        :param ind_node: indice du noeud à attribuer à la maille
-        '''
-        if self._nodes_belonging_to_cell[ind_cell] is not None:
-            self._nodes_belonging_to_cell[ind_cell] = np.append(self._nodes_belonging_to_cell[ind_cell], ind_node)
-        else:
-            self._nodes_belonging_to_cell[ind_cell] = np.array([ind_node])
-
-    def setCellsInContactWithNode(self, ind_node, ind_cell_list):
-        '''
-        Attribue la liste des indices des mailles, 'ind_cell_list'
-        au noeud d'indice 'ind_node'
-
-        :param ind_node: indice du noeud auquel attribuer les mailles
-        :param ind_cell_list: liste des indices des mailles à attribuer au noeud
-
-        :type ind_node: int
-        :type ind_cell_list: list
-        '''
-        self._cells_in_contact_with_node[ind_node] = np.array(ind_cell_list)
-
     def addCellInContactWithNode(self, ind_node, ind_cell):
         '''
         Ajoute l'indice, 'ind_cell', de la maille à la liste des mailles en contact
         avec le noeud d'indice 'ind_node'
         '''
-        if self._cells_in_contact_with_node[ind_node] is not None:
-            self._cells_in_contact_with_node[ind_node] = np.append(self._cells_in_contact_with_node[ind_node], ind_cell)
-        else:
-            self._cells_in_contact_with_node[ind_node] = np.array([ind_cell])
+        k = 0
+        while self._cells_in_contact_with_node[ind_node, k] != -1:
+            k += 1
+        self._cells_in_contact_with_node[ind_node, k] = ind_cell
 
     def getNodesBelongingToCell(self, ind_cell):
         '''
