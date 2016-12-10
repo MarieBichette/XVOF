bd12c0c8eff3800ccae9599fe7ed6ad7fe4be05b
hippo91 <guillaume.peillex@gmail.com>
2016-12-10 16:53:16 +0100

    self.__nodes_belonging_to_cell et self.__cells_in_contact_with_node ne sont plus des listes. Ce sont des tableaux numpy de shape (nbr_noeuds ou cells, 2) Suppression de méthodes non utilisées Ajout de doctest

--- xvof/mesh/topology1d.py
+++ xvof/mesh/topology1d.py
@@ -3,15 +3,44 @@
 """
 Classe gérant la topologie 1D du maillage
 """
+import numpy as np
+
 from xvof.mesh.topology import Topology
 
 
 class Topology1D(Topology):
-    '''
+    """
     Spécialisation 1D de topology
-    '''
+
+    >>> my_topo = Topology1D(11, 10)
+    >>> my_topo.getCellsInContactWithNode(0)
+    array([ 0, -1])
+    >>> my_topo.getCellsInContactWithNode(5)
+    array([4, 5])
+    >>> my_topo.getNodesBelongingToCell(9)
+    array([ 9, 10])
+    >>> my_topo.getCellsInContactWithNode(np.array([1, 3]))
+    array([[0, 1],
+           [2, 3]])
+    >>> my_topo.cells_in_contact_with_node[:]
+    array([[ 0, -1],
+           [ 0,  1],
+           [ 1,  2],
+           [ 2,  3],
+           [ 3,  4],
+           [ 4,  5],
+           [ 5,  6],
+           [ 6,  7],
+           [ 7,  8],
+           [ 8,  9],
+           [ 9, -1]])
+    """
     def __init__(self, nbr_of_nodes, nbr_of_cells):
         Topology.__init__(self, nbr_of_nodes, nbr_of_cells)
+        self._nodes_belonging_to_cell = np.ndarray(shape=(nbr_of_cells, 2), dtype=np.int64, order='C')
+        self._nodes_belonging_to_cell[:, :] = -1
+        self._cells_in_contact_with_node = np.ndarray(shape=(nbr_of_nodes, 2), dtype=np.int64, order='C')
+        self._cells_in_contact_with_node[:, :] = -1
         self._generateMesh(nbr_of_cells)
 
     def _generateMesh(self, nbr_of_cells):
@@ -24,21 +53,3 @@ class Topology1D(Topology):
             self.addCellInContactWithNode(ind_node_left, ind_cell)
             self.addCellInContactWithNode(ind_node_right, ind_cell)
             self.setNodesBelongingToCell(ind_cell, [ind_node_left, ind_node_right])
-
-    def isRightBoundary(self, node):
-        '''
-        Renvoie vrai si le noeud 'node' est le noeud frontiere droite
-        '''
-        if node.index == len(self._nodes) - 1:
-            return True
-        else:
-            return False
-
-    def isLeftBoundary(self, node):
-        '''
-        Renvoie vrai le noeud 'node' est le noeud frontiere gauche
-        '''
-        if node.index == 0:
-            return True
-        else:
-            return False
