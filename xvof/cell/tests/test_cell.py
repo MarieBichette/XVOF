1400add48a9787337db3776b1513cdac5da0980f
hippo91 <guillaume.peillex@gmail.com>
2016-12-27 19:20:48 +0100

    Correction/Update of the test case

--- xvof/cell/tests/test_cell.py
+++ xvof/cell/tests/test_cell.py
@@ -1,53 +1,78 @@
 #!/usr/bin/env python2.7
 # -*- coding: iso-8859-1 -*-
 """
-Classe de test du module cell
+Cell module unit tests
 """
 import numpy as np
 import unittest
 
-from xvof.miscellaneous import geometrical_props, material_props
-from xvof.miscellaneous import numerical_props, properties
-
-import xvof.cell.cell as el
-from xvof.equationsofstate import MieGruneisen
+from xvof.cell.cell import Cell
+from xvof.mesh.topology1d import Topology1D
 from xvof.node import OneDimensionNode
+from xvof.utilities.testing import captured_output
 
 
-class ElementTest(unittest.TestCase):
+class CellTest(unittest.TestCase):
 
     def setUp(self):
-        """ Préparation des tests """
-        equation_detat = MieGruneisen()
-        num_props = numerical_props(0.2, 1.0, 0.35)
-        mat_props = material_props(1.0e+05, 0.0, 8129., equation_detat)
-        geom_props = geometrical_props(1.0e-06)
-        props = properties(num_props, mat_props, geom_props)
-        noda = OneDimensionNode(1, np.array([-0.5]))
-        nodb = OneDimensionNode(2, np.array([0.1]))
-        self.my_elem = el.Cell(props, 1, [noda, nodb])
+        """
+        Tests setup
+        """
+        self.my_cell = Cell(3)
 
     def tearDown(self):
         pass
 
-    def test_coord(self):
-        """ Test de la méthode Element.coord() """
-        np.testing.assert_array_equal(self.my_elem.coord, np.array([-0.2]))
+    def test_get_coordinates(self):
+        """
+        Test of cell.get_coordinates method
+        """
+        nodes = OneDimensionNode(4, np.array([[-0.5], [0.1], [0.2], [0.35]]), np.array([[0.], [0.], [0.], [0.]]))
+        my_topo = Topology1D(4, 3)
+        cell_coord = Cell.get_coordinates(3, my_topo, nodes.xt)
+        np.testing.assert_array_almost_equal(cell_coord, np.array([[-0.2], [0.15], [0.275]]))
 
-    def test_incrementer(self):
-        """ Test de la méthode Element.increment() """
-        self.my_elem._pression_t_plus_dt = 2.0e+09
-        self.my_elem._rho_t_plus_dt = 8500.
-        self.my_elem._cson_t_plus_dt = 4360.
-        self.my_elem._nrj_t_plus_dt = 1.0e+06
-        self.my_elem._size_t_plus_dt = 0.015
-        self.my_elem.incrementer()
-        self.assertEqual(self.my_elem.pression_t, 2.0e+09)
-        self.assertEqual(self.my_elem.rho_t, 8500.)
-        self.assertEqual(self.my_elem.cson_t, 4360.)
-        self.assertEqual(self.my_elem.nrj_t, 1.0e+06)
-        self.assertEqual(self.my_elem.taille_t, 0.015)
+    def test_increment_variables(self):
+        """
+        Test of cell.increment_variables method
+        """
+        self.my_cell.pressure.new_value = np.array([2.0e+09, -0.5e+09, 1.25e+09])
+        self.my_cell.density.new_value = np.array([8500., 4500, 9500.])
+        self.my_cell.sound_velocity.new_value = np.array([440., 210, -110])
+        self.my_cell.energy.new_value = np.array([-1.0e+06, 0.5e+05, 1.5e+05]) 
+        self.my_cell._size_t_plus_dt = np.array([0.015, 0.01, 0.05])
+        self.my_cell.increment_variables()
+        np.testing.assert_array_equal(self.my_cell.pressure.current_value, np.array([2.0e+09, -0.5e+09, 1.25e+09]) )
+        np.testing.assert_array_equal(self.my_cell.density.current_value, np.array([8500., 4500, 9500.]))
+        np.testing.assert_array_equal(self.my_cell.sound_velocity.current_value, np.array([440., 210, -110]))
+        np.testing.assert_array_equal(self.my_cell.energy.current_value, np.array([-1.0e+06, 0.5e+05, 1.5e+05]) )
+        np.testing.assert_array_equal(self.my_cell.size_t, np.array([0.015, 0.01, 0.05]))
+
+    def test_print_infos(self):
+        """
+        Test of cell.print_infos method
+        """
+        self.my_cell.pressure.new_value = np.array([2.0e+09, -0.5e+09, 1.25e+09])
+        self.my_cell.density.new_value = np.array([8500., 4500, 9500.])
+        self.my_cell.sound_velocity.new_value = np.array([440., 210, -110])
+        self.my_cell.energy.new_value = np.array([-1.0e+06, 0.5e+05, 1.5e+05]) 
+        self.my_cell._size_t_plus_dt = np.array([0.015, 0.01, 0.05])
+        with captured_output() as (out, err):
+            self.my_cell.print_infos()
+            output = out.getvalue().strip()
+            answer = """Cell 
+            ==> number of cells = 3
+            ==> size at t = [ 0.  0.  0.]
+            ==> size at t+dt = [ 0.015  0.01   0.05 ]
+            ==> density at t = [ 8129.  8129.  8129.]
+            ==> density at t+dt = [ 8500.  4500.  9500.]
+            ==> pressure at t = [ 100006.2096  100006.2096  100006.2096]
+            ==> pressure at t+dt = [  2.00000000e+09  -5.00000000e+08   1.25000000e+09]
+            ==> internal energy at t = [ 7.689  7.689  7.689]
+            ==> internal energy at t+dt = [-1000000.    50000.   150000.]
+            ==> sound velocity at t = [ 0.  0.  0.]
+            ==> sound velocity at t+dt = [ 440.  210. -110.]"""
+            self.assertEqual(output.split(), answer.split())
 
 if __name__ == "__main__":
-    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
