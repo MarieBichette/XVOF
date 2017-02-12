bbe23dbcb983d79074e18712371989a0740cfa87
guillaume2 <guillaume.peillex@gmail.col>
2017-02-12 21:51:39 +0100

    No use of the superclass that has been deleted. Computation of the inverse is made right after computing the mass matrix. The inverse is stored to avoid any superfluous computation. The computation of the inverse was made at each cycle in Mesh1DEnriched ==> large amount of computation time spared!

--- xvof/mass_matrix/one_dimension_mass_matrix.py
+++ xvof/mass_matrix/one_dimension_mass_matrix.py
@@ -1,34 +1,48 @@
-#!/usr/bin/env python2.7
 # -*- coding: iso-8859-1 -*-
 """
-A class defining the mass matrix 1D
+Implementing the OneDimensionMassMatrix class
 """
 
 import numpy as np
+from xvof.mass_matrix.mass_matrix import compute_wilkins_mass_matrix
+from xvof.mass_matrix.mass_matrix_utilities import inverseMasse
 
-from xvof.mass_matrix.mass_matrix import MassMatrix
-
-
-class OneDimensionMassMatrix(MassMatrix):
+class OneDimensionMassMatrix(object):
     """
-    A class for mass matrix 1d
+    A class for 1d mass matrix
     """
 
     def __init__(self, number_of_nodes):
-        MassMatrix.__init__(self, number_of_nodes)
-        self._inv_mass_matrix = np.zeros([self.number_of_nodes, 1], dtype=np.float64, order='C')
+        self.__number_of_nodes = number_of_nodes
+        self.__mass_matrix = np.zeros([self.__number_of_nodes, 1], dtype=np.float64, order='C')
+        self.__inv_mass_matrix = None
 
-    def compute_mass_matrix(self, topologie, vecteur_masse_elements, vecteur_nb_noeuds_par_element):
+    def compute_mass_matrix(self, topology, cell_mass_vector, node_number_by_cell_vector):
         """
-        Compute the mass matrix (methode de Wilkins)
-        self.mass_matix is type array 1D (= vecteur)
+        Compute the mass matrix and its inverse according to Wilkins method
+
+        :param topology: topology of the simulation
+        :param cell_mass_vector: cells mass vector
+        :param node_number_by_cell_vector: number of nodes per cell (vector)
         """
-        self._mass_matrix = MassMatrix.compute_mass_matrix(self, topologie, vecteur_masse_elements,
-                                                           vecteur_nb_noeuds_par_element)
+        self.__mass_matrix = compute_wilkins_mass_matrix(topology, cell_mass_vector,
+                                                         node_number_by_cell_vector)
+        self.__inv_mass_matrix = inverseMasse(self.__mass_matrix)
 
     @property
-    def mass_matrix_value(self):
-        """Accessor on _mass_matrix"""
-        return self._mass_matrix
+    def inverse_mass_matrix(self):
+        """
+        Accessor on the inverse of the mass matrix
+
+        :return: the inverse of the mass matrix
+        """
+        return self.__inv_mass_matrix
+
+    @property
+    def mass_matrix(self):
+        """
+        Accessor on _mass_matrix
+        """
+        return self.__mass_matrix
 
 
