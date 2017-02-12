1b860311dcef49b191b301c2551c49e30472c34e
guillaume2 <guillaume.peillex@gmail.col>
2017-02-12 21:47:26 +0100

    No need to have that class. A simple function is enough.

--- xvof/mass_matrix/mass_matrix.py
+++ xvof/mass_matrix/mass_matrix.py
@@ -1,53 +1,28 @@
-#!/usr/bin/env python2.7
 # -*- coding: iso-8859-1 -*-
 """
-A class defining the mass matrix
+Implementing the MassMatrix class
 """
 import numpy as np
 
-from abc import abstractmethod
 
-from xvof.mass_matrix.mass_matrix_utilities import inverseMasse
-
-class MassMatrix(object):
+def compute_wilkins_mass_matrix(topology, cell_mass_vector, node_number_by_cell_vector):
     """
-    Class for mass matrix (general)
+    Compute nodal mass by averaging the mass of neighbouring cells (Wilkins method)
+
+    :param topology: topology of the simulation
+    :param cell_mass_vector: cells mass vector
+    :param node_number_by_cell_vector: number of nodes per cell (vector)
+
+    :type topology: Topology
+    :type cell_mass_vector: numpy.array([nbr_of_nodes, 1], dtype=np.float64, order='C')
+    :type node_number_by_cell_vector: numpy.array([nbr_of_nodes, 1], dtype=np.int64, order='C')
     """
-    def __init__(self,number_of_nodes):
-        self.number_of_nodes = number_of_nodes
-        # import ipdb ; ipdb.set_trace()
-        self._mass_matrix = np.zeros([self.number_of_nodes, 1], dtype=np.float64, order='C')
-
-
-    def compute_mass_matrix(self, topologie, vecteur_masse_elements, vecteur_nb_noeuds_par_element):
-        """
-        Calcule les masses associées à chaque noeud par moyenne arithmétique de la
-        masse des éléments voisins (méthode Wilkins)
-        self.mass_matix is type array 1D (= vecteur)
-
-        :param topologie: topologie du calcul
-        :param vecteur_masse_elements: vecteur des masses de chaque élément
-        :param vecteur_nb_noeuds_par_element: vecteur des nombres de noeuds que possède chaque élément
-
-        :type topologie: Topology
-        :type vecteur_masse_elements: numpy.array([nbr_of_nodes, 1], dtype=np.float64, order='C')
-        :type vecteur_nb_noeuds_par_element: numpy.array([nbr_of_nodes, 1], dtype=np.int64, order='C')
-        """
-        for ind_node in xrange(self.number_of_nodes):
-            elements_voisins = topologie.getCellsInContactWithNode(ind_node)
-            # Indice -1 => element inexistant
-            elements_voisins = elements_voisins[elements_voisins != -1]
-            self._mass_matrix[ind_node] = np.sum(vecteur_masse_elements[elements_voisins] /
-                                           vecteur_nb_noeuds_par_element[elements_voisins])
-        return self._mass_matrix
-
-
-    @property
-    def mass_matrix_value(self):
-        """Accessor on _mass_matrix"""
-        return self._mass_matrix
-
-
-
-
-
+    nbr_nodes = len(topology.cells_in_contact_with_node)
+    mass_matrix = np.zeros([nbr_nodes, 1], dtype=np.float64, order='C')
+    for ind_node in xrange(nbr_nodes):
+        neighboring_cells = topology.getCellsInContactWithNode(ind_node)
+        # Index -1 => unexistant cell
+        neighboring_cells = neighboring_cells[neighboring_cells != -1]
+        mass_matrix[ind_node] = np.sum(
+            cell_mass_vector[neighboring_cells] / node_number_by_cell_vector[neighboring_cells])
+    return mass_matrix
