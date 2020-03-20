3acf6e05f036f53b970f71bf6dcc4ae7bb02d8fd
hippo91 <guillaume.peillex@gmail.com>
2020-03-20 20:04:29 +0100

    Add pylint in travis

---
+++ pylint-pre-hook.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python
+"""Git pre-commit hook to run pylint on python files.
+
+Slight variation of: 
+    https://gist.github.com/nivbend/7e0e306a98138916b3c9#file-run_pylint-py
+    
+Copy it under .git/hooks/pre-commit at the root of your project.
+"""
+
+from __future__ import print_function
+from subprocess import check_output, CalledProcessError
+from sys import stderr
+from os.path import isfile
+
+(SUCCESS,
+ GIT_DIFF_ERROR,
+ PYLINT_ERRORS) = range(3)
+
+PYLINTRC = ".pylintrc"
+
+def _print_error(message):
+    """Print an error message to stderr."""
+    print(message, file = stderr)
+
+def _is_python_script(filename):
+    """Return true for *.py files and python scripts ("#! /path/to/python")."""
+    if not isfile(filename):
+        return False
+
+    if not filename.endswith(".py"):
+        try:
+            with open(filename, "rb") as contents:
+                first_line = contents.readline()
+        except OSError:
+            return False
+
+        # Check shebang.
+        if not (first_line.startswith("#!") and "python" in first_line):
+            return False
+
+    return True
+
+def run():
+    """Verify changed python files using pylint."""
+    # Get all changed files' paths.
+    try:
+        changed_files = check_output(["git", "diff", "--staged", "--name-only", "HEAD", ])
+    except CalledProcessError:
+        _print_error("Couldn't get list of changed files")
+        return GIT_DIFF_ERROR
+
+    # Limit checks to python scripts only.
+    changed_files = [
+        filename for filename
+        in changed_files.splitlines()
+        if _is_python_script(filename)]
+
+    if changed_files:
+        cmd = ["pylint",]
+        if isfile(PYLINTRC):
+            cmd += ["--rcfile={}".format(PYLINTRC)]
+        try:
+            check_output(cmd + changed_files)
+        except CalledProcessError as error:
+            _print_error(error.output)
+            _print_error("pylint returned errors, aborting commit.")
+            return PYLINT_ERRORS
+
+    return SUCCESS
+
+if __name__ == "__main__":
+    exit(run())
