c37efedbc062c8fc4249c0880dab6645dfea15f4
hippo91 <guillaume.peillex@gmail.com>
2020-03-20 11:28:07 +0100

    The package are automatically found thanks to find_packages function

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python
+"""
+Setup script for XtendedFiniteVolume project
+"""
 
 from distutils.core import setup
+import os
+import os.path
 
-setup(name='XFV',
+
+def find_packages():
+    """
+    Return the list of all packages
+    """
+    packages = []
+    for (dirpath, _, _) in os.walk('xfv'):
+        if os.path.exists(os.path.join(dirpath, '__init__.py')):
+            packages.append(dirpath.replace('/', '.'))
+    return packages
+
+
+setup(name='XtendedFiniteVolume',
       version='1.0',
       description='A 1D hydrocode with discontinuous fields abilities',
       author='Guillaume Peillex',
@@ -10,11 +27,6 @@ setup(name='XFV',
       maintainer='Guillaume Peillex',
       maintainer_email='guillaume.peillex@gmail.com',
       url='https://github.com/hippo91/XVOF',
-      packages=['xvof.src', 'xvof.src.boundary_condition', 'xvof.src.cell', 'xvof.src.cohesive_model',
-                'xvof.src.contact', 'xvof.src.data', 'xvof.src.discontinuity', 'xvof.src.equationsofstate',
-                'xvof.executables', 'xvof.src.fields', 'xvof.src.figure_manager', 'xvof.src.mass_matrix',
-                'xvof.src.mesh', 'xvof.src.node', 'xvof.src.output_figure', 'xvof.src.output_manager',
-                'xvof.src.plasticitycriterion', 'xvof.src.pressurelaw', 'xvof.src.rheology', 'xvof.src.rupturecriterion',
-                'xvof.src.rupturetreatment', 'xvof.src.solver', 'xvof.src.utilities'],
-        scripts=['xvof/executables/VNR_1D_EnrichedElement.py']
+      packages=find_packages(),
+      scripts=['xfv/XtendedFiniteVolume.py']
      )
