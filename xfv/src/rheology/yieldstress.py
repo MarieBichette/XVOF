43e831e81711aea02bbc7e7f1658a4d7889623b2
mgorecki-work <marie.gorecki@wanadoo.fr>
2020-03-24 11:59:27 +0100

    Pylint

--- xfv/src/rheology/yieldstress.py
+++ xfv/src/rheology/yieldstress.py
@@ -5,11 +5,15 @@ Implementation d'une classe de limite d'élasticité (interface)
 """
 
 
-class YieldStress(object):
+class YieldStress(object):  # pylint: disable=too-few-public-methods
     """
     Interface de limite d'élasticité
     """
-    def __init__(self, initial_value, **kwargs):
+    def __init__(self, initial_value):
+        """
+        Initialization of the class
+        :param initial_value: yield_stress initial value
+        """
         self.yield_stress = initial_value
 
     @classmethod
