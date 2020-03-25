ce99383e80743da497c24ef272fc79d320bfb0b1
mgorecki-work <marie.gorecki@wanadoo.fr>
2020-03-25 15:51:46 +0100

    Ajout d'un test pour vérifier qu'un cas test est bien fourni en argument du script

--- xfv/post_processing/field_evolution_in_time.py
+++ xfv/post_processing/field_evolution_in_time.py
@@ -1,136 +1,81 @@
-#!/usr/bin/env python2.7
-# -*- coding: iso-8859-15 -*-
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
 """ 
-Compare the time evolution of nodes and cell for xfem simulation with reference
-Parameters :
-    USER INPUT :
-    id_item : id of the item to be studied (int)
-        For 'max_diff_vs_space' : possibility to consider a list of ids : id_item = -1
-            -> takes into account a list entered in the source code
-    case_list : list of cases to be considered.
-            case : tuple qui contient : nom du cas, simulation associée,
-                    répertoire de stockage des résultats, label et couleur pour les graphiques
-            entrer cette liste entre guillemet et sans espace. séparateur = ','.
-            case default : pour prendre les résultats situés dasn "0_XFEM"
-
-    OTHER PARAMETERS
-    save_fig, show_fig : booléens pour sauver et afficher les résultats
-    id_item_list : cf id_item
-    ref_case : default : mass_wilkins
+Plot time evolution of a field for a given item id
 """
 
-import os
-import sys
+import argparse
+import pathlib
 import matplotlib.pyplot as plt
 
-import xfv.src.figure_manager.time_figure_tools as fig_tools_path
-from xfv.src.utilities.case_definition import CaseManager
-from xfv.src.figure_manager.time_figure_manager import TimeFigureManager
-from xfv.src.figure_manager.time_figure_tools import TimeFigureTools
-from xfv.src.output_figure.profile_tools import A3_list, plot_field_from_txt_file, get_field_from_txt_file
+from xfv.post_processing.hdf5_postprocessing_tools import get_field_evolution_in_time_for_item
 
-# -----------------------------------------
-# Initialize treatment and legend
-# -----------------------------------------
-save_fig = True
-show_fig = True
-file_name = "all_fields.hdf5"
-extension = '.hdf5'
+# ----------------------------------------------------------
+# Read instructions
+# ----------------------------------------------------------
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", action="store_true", help="Increase program verbosity")
+parser.add_argument("field", help="the field to be plotted")
+parser.add_argument("item_id", type=int, help="the id of the item to look at")
+parser.add_argument("-case", action='append', nargs='+',
+                    help="the path to the output repository")
+parser.add_argument("--output_filename", default="all_fields.hdf5",
+                    help="the name of the output hdf5 band (default = all_fields.hdf5)")
+args = parser.parse_args()
 
-project_dir = os.path.split(os.path.dirname(os.path.abspath(os.path.curdir)))[0]
-base_dir = os.path.split(project_dir)[0]
+if args.case is None:
+    raise ValueError("At least ont case is needed. Use -case to specify cases to plot")
 
-# -----------------------------------------
-# Read user instructions
-# -----------------------------------------
-msg = "Mini programme pour tracer l'évolution temporelle d'un champ en un point de la barre\n" \
-      "Ce script attend comme arguments  : \n"
-msg += "- le type de simulation (single|compare|diff) \n"
-msg += "- les champs à tracer ([X]Field, ...)\n"
-msg += "- les ids des items (int, séparés par une virgule et sans espace) \n"
-msg += "- une liste des cas à traiter (séparés par une virgule, sans espace | None par défaut) \n"
-msg += "- -h ou --help pour afficher l'aide\n"
-msg += " Si analysis = diff : " \
-    "\n \t - Le premier cas rensigné sert de référence" \
-    "\n \t - On demandera une option pour savoir comment calculer l'erreur (absolue, adimensionnée, relative)"
+if args.verbose:
+    print("Field : " + args.field)
+    print("Item id : " + str(args.item_id))
+    print("Cases : ")
+    print(args.case)
+    print("~~~~~~~~~~~~~")
 
-analysis = None
+item_id = args.item_id
+field = args.field
+complete_field_name = field + "Field"
 
-if len(sys.argv) not in [2, 5]:
-    print(msg)
-    exit(0)
+# ----------------------------------------------------------
+# Prepare figure
+# ----------------------------------------------------------
+field_unit = dict()
+field_unit["Density"] = "[$kg/m^3$]"
+field_unit["Pressure"] = "[$Pa$]"
+field_unit["PseudoViscosity"] = "[$Pa$]"
+field_unit["InternalEnergy"] = "[$J/kg$]"
+field_unit["Velocity"] = "[$ms/s$]"
 
-if sys.argv[1] in ["-h", "--help"]:
-    print(msg)
-    exit(0)
+plt.figure(1)
+plt.xlabel("Time [$\mu$s]")
+plt.ylabel(field + field_unit[field])
+plt.title("Time evolution of " + str(field) + " in item " + str(item_id), fontweight='bold')
 
-# on lit lest arguments données par l'utilisateur
-try:
-    analysis = sys.argv[1]
-    if analysis not in ['single', 'compare', 'diff']:
-        print(msg)
-        print(("Type d'analysis {:s} inconnu!".format(analysis)))
-        os._exit(0)
+# ----------------------------------------------------------
+# Plot field evolution for each case
+# ----------------------------------------------------------
+for case in args.case[0]:
+    if args.verbose:
+        print("Case is : " + case)
+    path_to_db = pathlib.Path.cwd().joinpath("..", "tests", case, args.output_filename)
+    if args.verbose:
+        print("Path to database : {:}".format(path_to_db))
+        print("Read field " + field + " in database... ")
+    # Read database :
+    item_history = get_field_evolution_in_time_for_item(path_to_db, item_id, field)
+    if args.verbose:
+        print("Done !")
+        print("~~~~~~~~~~~~~")
+    # Plot field :
+    plt.plot(item_history[:, 0] * 1.e+6, item_history[:, 1], label=case)
 
-    field_list = sys.argv[2].split(',')
-    field_list = [getattr(fig_tools_path, t_f) for t_f in field_list]
+# ----------------------------------------------------------
+# Show figure
+# ----------------------------------------------------------
+plt.legend(loc="best")
+plt.show()
 
-    id_item_list = sys.argv[3].split(',')
-    id_item_list = [int(id_item) for id_item in id_item_list]
-
-except:
-    print(msg)
-    raise
-
-case_list = sys.argv[4].split(',')
-case_list = [CaseManager().find_case(case) for case in case_list]
-case_ref_choice = case_list[0]  # pas utile pour une comparaison single mais on a beosin d'en définir un quand même
-
-if analysis in ['diff']:
-    # Pour une courbe d'erreur, la référence est donnée par le premier cas renseigné
-    if len(case_list) < 2:
-        print("Impossible de faire une analyse en erreur (diff) si il n'y a pas au moins 2 cas renseignés")
-        exit(0)
-    case_ref_choice = case_list[0]
-    case_list = case_list[1:]
-    # On demande quel calcul d'erreur faire
-    option = 1
-    # option = None
-    # while option not in range(3):
-    #     option = input("Quelle méthode de calcul pour l'erreur ? (0 = absolue, 1 = adimensionnée, 2 = relative) ")
-
-
-
-# -----------------------------------------
-# Run user instruction (analysis)
-# -----------------------------------------
-for case in case_list:
-    print("-------------------------------")
-    print(case.case_name)
-    item = field_list[0].application
-
-    for field in field_list:
-        print(field.title)
-
-        for id_item in id_item_list:
-            print("item : {:}".format(id_item))
-
-            fig_manager = TimeFigureManager(case, item, id_item, data_file_name=file_name, case_ref=case_ref_choice)
-            fig_tools = TimeFigureTools(case, item, id_item, data_file_name=file_name)
-
-            if analysis == 'single':
-                fig_manager.plot_single_time_figure(field)
-                plt.legend(loc="best")
-
-            elif analysis == 'diff':
-                print("Reference case is {}".format(case_ref_choice.case_name))
-                print("Post treating data for case : {:}".format(case.label))
-                fig_manager.plot_error_vs_time(field, compute_error_index=option)
-                # set_acceptance_criterion(item, id_item, 0., 5., 1.e-4)
-                plt.legend(loc=4)
-
-if show_fig:
-    plt.show()
 
 
 
--- xfv/post_processing/free_surface_velocity.py
+++ xfv/post_processing/free_surface_velocity.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env python2.7
-# -*- coding: iso-8859-15 -*-
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
 """ 
-Plot the free surface velocity
+Plot the free surface velocity eventually with experimental data
 """
 
 import argparse
+import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
 
+from xfv.post_processing.hdf5_postprocessing_tools import get_field_evolution_in_time_for_item
 
 # ----------------------------------------------------------
 # Read instructions
@@ -17,12 +19,12 @@ parser.add_argument("-v", "--verbose", action="store_true", help="Increase progr
 parser.add_argument("-case", action='append', nargs='+',
                     help="the path to the output repository")
 parser.add_argument("-experimental_data", help="the path to experimental data to plot")
-parser.add_argument("-shif_experimental_data", type=float, default=0,
-                    help="to shift the experimental data in time in order to fit data "
-                         "(default = 0)")
 parser.add_argument("--output_filename", default="all_fields.hdf5",
                     help="the name of the output hdf5 band (default = all_fields.hdf5)")
 args = parser.parse_args()
+
+if args.case is None:
+    raise ValueError("At least ont case is needed. Use -case to specify cases to plot")
 
 if args.verbose:
     print("Cases : ")
@@ -31,20 +33,18 @@ if args.verbose:
         print("Experimental data : " + args.experimental_data)
     print("~~~~~~~~~~~~~")
 
-complete_field_name = field + "Field"
 exp_data = args.experimental_data
-shift_data = args.shif_experimental_data
 
 # ----------------------------------------------------------
 # Prepare figure
 # ----------------------------------------------------------
 plt.figure(1)
-plt.title("Evolution of the free surface velocity", fontsize=20, fontweight='bold')
+plt.title("Evolution of the free surface velocity", fontweight='bold')
 plt.xlabel("Time [$\mu$s]")
-plt.ylabel("Velocity of free surface [m/s]")
+plt.ylabel("Free surface velocity [m/s]")
 
 # ----------------------------------------------------------
-# Plot each case
+# Plot free surface velocity for each case
 # ----------------------------------------------------------
 for case in args.case[0]:
     if args.verbose:
@@ -52,98 +52,32 @@ for case in args.case[0]:
     path_to_db = pathlib.Path.cwd().joinpath("..", "tests", case, args.output_filename)
     if args.verbose:
         print("Path to database : {:}".format(path_to_db))
-        print("Read field " + field + " in database... ")
+        print("Read VelocityField in database... ")
     # Read database :
+    # Free surface is the last node => index -1 in Numpy array
     item_history = get_field_evolution_in_time_for_item(path_to_db, -1, "NodeVelocity")
     if args.verbose:
         print("Done !")
         print("~~~~~~~~~~~~~")
-    # Plot field :
-    plt.plot(item_history[:, 0] * 1.e+6, item_history[:, 1], label=case)
+    # Plot velocity with t0 = detection of free surface movement:
+    time = item_history[:, 0]
+    velocity = item_history[:, 1]
+    time_0 = 0.
+    # Uncomment to shift free surface velocity for plate impact cases
+    # velocity_is_not_zero = np.where(velocity > 1)  # 1 m/s is ok to detect non zero velocity
+    # time_0 = time[np.where(velocity_is_not_zero)[0]][0]  # first time where non zero velocity
+    plt.plot((time - time_0) * 1.e+6, velocity, label=case)
+
+# ----------------------------------------------------------
+# Plot experimental data
+# ----------------------------------------------------------
+if exp_data is not None:
+    experimental_velocity = np.loadtxt(exp_data)
+    plt.plot(experimental_velocity[:, 0], experimental_velocity[:, 1], "--",
+             color="black", label="Experiment")
 
 # ----------------------------------------------------------
 # Show figure
 # ----------------------------------------------------------
-
-
-
-project_dir = os.path.split(os.path.dirname(os.path.abspath(os.path.curdir)))[0]
-base_dir = os.path.split(project_dir)[0]
-
-recalage_temps = True
-id_item = -1  # dernier noeud (surface libre du bord opposé)
-
-experimental_file = "TA6.TXT"
-experimental_dir = os.path.join(base_dir, "Documents/These/Experimental_data/Ta/")
-
-exp_data = os.path.join(experimental_dir, experimental_file)
-
-hdf5_band = "all_fields.hdf5"
-
-fig = plt.figure(VelocityField.colonne_history)
-fig.patch.set_facecolor("white")
-if english:
-
-else:
-    fig.suptitle("Vitesse de surface libre", fontsize=20, fontweight='bold')
-    # plt.xlabel("Temps [$\mus$]", fontsize=18)
-    plt.ylabel("Vitesse de la surface libre [m/s]", fontsize=18)
-
-# -----------------------------------------
-# Read user instructions
-# -----------------------------------------
-msg = "Petit programme tout mignon pour tracer la vitesse de surface libre simulée et expérimentale \n"
-msg += "Ce script attend comme  arguments  : \n"
-msg += "- une liste des cas à traiter (séparés par une virgule, sans espace, None par défaut) \n"
-msg += "- -h ou --help pour afficher l'aide\n"
-msg += "On pose la question si on souhaite afficher les données expérimentales actuelles " \
-       "(adresse du fichier dans le code)"
-
-if len(sys.argv) != 2:
-    print(msg)
-    exit(0)
-
-if sys.argv[1] in ["-h", "--help"]:
-    print(msg)
-    exit(0)
-
-# choix_exp = raw_input("Voulez vous tracer les résultats expérimentaux du fichier {:} ? (y/n) ".format(exp_data))
-choix_exp = "y"
-
-case_list = sys.argv[1].split(',')
-case_list = [CaseManager().find_case(case) for case in case_list]
-
-# Define and plot experimental results
-if choix_exp == "y":
-    exp_result = ExperimentalData(exp_data)
-    exp_result.compute_spall_stress(rho_0=16960., c_0=3460., us1=274, us2=109)
-    # exp_result.compute_spall_stress(rho_0=8930., c_0=3940., us1=502, us2=431)
-    exp_result.plot_experimental_results(VelocityField.colonne_history)
-
-# -----------------------------------------
-# Run user instruction (analysis)
-# -----------------------------------------
-for case in case_list:
-    print("-------------------------------")
-    print(case.case_name)
-    # exp_result.plot_hugoniot(case)
-
-    # Tracé des données numériques
-    path_to_hdf5_db = os.path.join(case.directory_name, hdf5_band)
-    print("Read hdf5 data in file {:}".format(path_to_hdf5_db))
-
-    hdf5_data = read_database_in_array(path_to_hdf5_db, id_item, "NodeVelocity", 1)
-    time = hdf5_data[:, 0]
-    velocity = hdf5_data[:, 1]
-    mask = (velocity > 1)
-    time_0 = time[np.where(mask)[0][0]]
-
-    np.savetxt(os.path.join(case.directory_name, "vitesse_surface_libre.dat"), hdf5_data)
-
-    plt.figure(VelocityField.colonne_history)
-    case_label = "{:}".format(case.label)
-    plt.plot((time - time_0) * 1.e+06, velocity, color=case.color, marker=case.marker, label=case_label, linestyle=case.linestyle)
-    # plt.plot((time[mask] - time_0) * 1.e+06, velocity[mask], color=case.color, marker=case.marker, label=case.label)
-
 plt.legend(loc="best")
 plt.show()
