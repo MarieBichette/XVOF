e7a4d07b4bdc8d8f287c7eec240896110740fba3
hippo91 <guillaume.peillex@gmail.com>
2020-03-30 11:51:27 +0200

    Deletes a leading underscore on _unloading_model_class variable to make inheritance easier without name mangling

--- xfv/src/data/unloading_model_props.py
+++ xfv/src/data/unloading_model_props.py
@@ -15,25 +15,25 @@ from xfv.src.cohesive_model.loss_of_stiffness_unloading_model import LossOfStiff
 class UnloadingModelProps:  # pylint: disable=missing-class-docstring
     slope: float
     cohesive_strength: float
-    __unloading_model_class: Type[UnloadingModelBase] = field(init=False, repr=False)
+    _unloading_model_class: Type[UnloadingModelBase] = field(init=False, repr=False)
 
     def build_unloading_model_obj(self):
         """
         A factory that build and return the UnloadingModel object
         """
-        return self.__unloading_model_class(self.slope, self.cohesive_strength)
+        return self._unloading_model_class(self.slope, self.cohesive_strength)
 
 
 @dataclass  # pylint: disable=missing-class-docstring
 class ZeroForceUnloadingModelProps(UnloadingModelProps):
-    __unloading_model_class = ZeroForceUnloadingModel
+    _unloading_model_class = ZeroForceUnloadingModel
 
 
 @dataclass  # pylint: disable=missing-class-docstring
 class ProgressiveUnloadingModelProps(UnloadingModelProps):
-    __unloading_model_class = ProgressiveUnloadingModel
+    _unloading_model_class = ProgressiveUnloadingModel
 
 
 @dataclass  # pylint: disable=missing-class-docstring
 class LossOfStiffnessUnloadingModelProps(UnloadingModelProps):
-    __unloading_model_class = LossOfStiffnessUnloadingModel
+    _unloading_model_class = LossOfStiffnessUnloadingModel
