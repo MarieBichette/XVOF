d4a6ac77f51744fa079e7a52a9627a4dcf98a972
mgorecki-work <marie.gorecki@wanadoo.fr>
2020-03-30 15:45:49 +0200

    Interface pour le contact

--- xfv/src/contact/contact_base.py
+++ xfv/src/contact/contact_base.py
@@ -1,76 +1,32 @@
-# -*- coding: iso-8859-1 -*-
+# -*- coding: utf-8 -*-
 """
 Definition of Contact management
 """
-from xfv.src.cohesive_model.cohesive_zone_model import CohesiveZoneModel
+from abc import abstractmethod
+import numpy as np
+
+from xfv.src.discontinuity.discontinuity import Discontinuity
 
 
-class ContactModel(object):
+class ContactBase(object):
     """
     An interface for all cohesive zone model
     """
-    def __init__(self, disc):
+    def __init__(self, *args, **kwargs):
         """
-        Constructeur :
-        :param sigma_0 : cohesive strength
-        :param separation_1 : ouverture au changement de pente
-        :param stress_1 : contrainte au changement de pente
-        :param delta_2 : ouverture critique (sigma = 0)
+        Build the contact model to prevent discontinuities boundaries interpenetration
         """
-        self.disc = disc
-        self._has_contact = False
-        self._lambda_multiplier = 0
-        self._node_mass_g = 0
-        self._node_mass_d = 0
+        pass
 
-    @property
-    def has_contact(self):
-        return self._has_contact
-
-    def compute_contact(self, node_velocity, time_step):
+    @abstractmethod
+    def apply_contact(self, node_coord: np.array, node_velocity: np.array, node_force: np.array,
+                      disc: Discontinuity, delta_t: float):
         """
-
-        :param node_velocity:
-        :param time_step:
-        :return:
+        Checks if contact and apply correction
+        :param node_coord: node coordinates array
+        :param node_velocity: node velocity array
+        :param node_force : node force array
+        :param disc: discontinuity to examine
+        :param delta_t : time step
         """
-        Mleft = self.disc.mass_matrix_enriched.get_mass_matrix_left()
-        Mright = self.disc.mass_matrix_enriched.get_mass_matrix_right()
-        M1 = Mleft[0, 0]
-        M2enr = Mleft[1, 1]
-        M2 = Mright[2, 2]
-        M1enr = Mright[3, 3]
-        epsilon = self.disc.discontinuity_position
-        self._node_mass_g = M1 * M2enr / (M2enr * (1-epsilon) + M1 * epsilon)
-        self._node_mass_d = M1enr * M2 / (M2 * (1-epsilon) + M1enr * epsilon)
-        ug = (1-epsilon) * node_velocity[self.disc.mask_in_nodes] + epsilon * self.disc.additional_dof_velocity_new[0]
-        ud = (1-epsilon) * self.disc.additional_dof_velocity_new[1] + epsilon * node_velocity[self.disc.mask_out_nodes]
-        denom = time_step * (self._node_mass_g + self._node_mass_d) / (self._node_mass_g * self._node_mass_d)
-        self._lambda_multiplier = (ug - ud).flatten() / denom
-
-    def apply_contact(self, node_velocity, time_step):
-        """
-
-        :param node_velocity:
-        :param time_step:
-        :return:
-        """
-        node_velocity[self.disc.mask_in_nodes] -= time_step * self._lambda_multiplier / self._node_mass_g
-        self.disc.additional_dof_velocity_new[1] -= time_step * (self._lambda_multiplier / self._node_mass_g).flatten()
-        node_velocity[self.disc.mask_out_nodes] += time_step * self._lambda_multiplier / self._node_mass_d
-        self.disc.additional_dof_velocity_new[0] += time_step * (self._lambda_multiplier / self._node_mass_d).flatten()
-
-    def check_contact(self, node_position):
-        """
-
-        :param node_position:
-        :return:
-        """
-        self.disc.compute_discontinuity_new_opening(node_position)
-        opening_old = self.disc.discontinuity_opening.current_value
-        opening_new = self.disc.discontinuity_opening.new_value
-        tol = 1.e-16
-        if opening_new < tol or opening_old < tol:
-            self._has_contact = True
-        else:
-            self._has_contact = False
+        pass
