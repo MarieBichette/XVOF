bdd56b14c775050a8a4e3edc3b0bc9ddb01d8643
mgorecki-work <marie.gorecki@wanadoo.fr>
2020-03-31 11:09:41 +0200

    Mouvement du calcul disc opening après calcul des tailles des mailles

--- xfv/src/mesh/mesh1denriched.py
+++ xfv/src/mesh/mesh1denriched.py
@@ -197,8 +197,9 @@ class Mesh1dEnriched(object):  # pylint:disable=too-many-instance-attributes, to
         self.nodes.compute_new_coodinates(delta_t)
         # TODO : ménage à faire
         self.nodes.enriched_nodes_compute_new_coordinates(delta_t)
-        for disc in Discontinuity.discontinuity_list():
-            disc.compute_discontinuity_new_opening(self.nodes.xtpdt)
+        # Discontinuity opening calculation cannot occur here because it needs the cracked cell
+        # part size which have not been updated yet. The discontinuity opening is computed after
+        # updating the cell size
 
     @timeit_file("/tmp/profil_xfv.src.txt")
     def compute_cells_sizes(self):
@@ -214,6 +215,9 @@ class Mesh1dEnriched(object):  # pylint:disable=too-many-instance-attributes, to
         """
         self.cells.compute_new_size(self.__topology, self.nodes.xtpdt, self.cells.classical)
         self.cells.compute_enriched_elements_new_part_size(delta_t, self.nodes.upundemi)
+        # Update discontinuity opening
+        for disc in Discontinuity.discontinuity_list():
+            disc.compute_discontinuity_new_opening(self.nodes.xtpdt)
 
     @timeit_file("/tmp/profil_xfv.src.txt")
     def compute_new_cells_densities(self):
