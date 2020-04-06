9bc2ffa192fc13a1f46957397f8fa0779562d5d4
hippo91 <guillaume.peillex@gmail.com>
2020-04-06 09:50:39 +0200

    Factorizes the computation of the specific volume

--- xfv/src/solver/functionstosolve/vnrenergyevolutionforveformulation.py
+++ xfv/src/solver/functionstosolve/vnrenergyevolutionforveformulation.py
@@ -12,6 +12,7 @@ class VnrEnergyEvolutionForVolumeEnergyFormulation(FunctionToSolveBase):
     Defines the evolution function of the internal energy that must vanish in VNR scheme
     [v, e] formulation
     """
+    def computeFunctionAndDerivative(self, var_value, mask):  # pylint: disable=too-many-locals
         _mask = np.where(mask)
         nrj = var_value[_mask]
         eos = self._variables['EquationOfState']
@@ -24,7 +25,7 @@ class VnrEnergyEvolutionForVolumeEnergyFormulation(FunctionToSolveBase):
         dpsurde = np.ndarray(nrj.shape, dtype=np.float64, order='C')
         eos.solve_volume_energy(new_spec_vol, nrj, p_i, dpsurde)
         # Function to vanish
-        delta_v = 1. / new_rho - 1. / old_rho
+        delta_v = new_spec_vol - 1. / old_rho
         func = nrj + p_i * delta_v / 2. + pressure * delta_v / 2. - old_nrj
         # Derivative of the function with respect to internal energy
         dfunc = 1 + dpsurde * delta_v / 2.
