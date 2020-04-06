2fde8d49a4370c0e5e84de3d2bb4a4d00bcdd8bb
hippo91 <guillaume.peillex@gmail.com>
2020-04-06 10:19:15 +0200

    Avoid copying arrays when using fancy indexing

--- xfv/src/solver/functionstosolve/vnrenergyevolutionforveformulation.py
+++ xfv/src/solver/functionstosolve/vnrenergyevolutionforveformulation.py
@@ -12,21 +12,21 @@ class VnrEnergyEvolutionForVolumeEnergyFormulation(FunctionToSolveBase):
     Defines the evolution function of the internal energy that must vanish in VNR scheme
     [v, e] formulation
     """
-    def computeFunctionAndDerivative(self, var_value, mask):  # pylint: disable=too-many-locals
+    def computeFunctionAndDerivative(self, var_value, mask):
         _mask = np.where(mask)
         nrj = var_value[_mask]
+        new_spec_vol = 1. / self._variables['NewDensity'][_mask]
+
         eos = self._variables['EquationOfState']
-        old_rho = self._variables['OldDensity'][_mask]
-        new_rho = self._variables['NewDensity'][_mask]
-        new_spec_vol = 1. / new_rho
-        pressure = self._variables['Pressure'][_mask]
-        old_nrj = self._variables['OldEnergy'][_mask]
         p_i = np.ndarray(nrj.shape, dtype=np.float64, order='C')
         dpsurde = np.ndarray(nrj.shape, dtype=np.float64, order='C')
+
         eos.solve_volume_energy(new_spec_vol, nrj, p_i, dpsurde)
         # Function to vanish
-        delta_v = new_spec_vol - 1. / old_rho
-        func = nrj + p_i * delta_v / 2. + pressure * delta_v / 2. - old_nrj
+        delta_v = new_spec_vol - 1. / self._variables['OldDensity'][_mask]
+        func = (nrj + p_i * delta_v / 2. +
+                self._variables['Pressure'][_mask] * delta_v / 2. -
+                self._variables['OldEnergy'][_mask])
         # Derivative of the function with respect to internal energy
         dfunc = 1 + dpsurde * delta_v / 2.
         return func, dfunc
