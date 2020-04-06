7db66358be6a2be65b9d17ff1180de665b3ca2d5
hippo91 <guillaume.peillex@gmail.com>
2020-04-06 09:49:40 +0200

    Uses indexes instead of bool for fancy indexing

--- xfv/src/solver/functionstosolve/vnrenergyevolutionforveformulation.py
+++ xfv/src/solver/functionstosolve/vnrenergyevolutionforveformulation.py
@@ -12,9 +12,14 @@ class VnrEnergyEvolutionForVolumeEnergyFormulation(FunctionToSolveBase):
     Defines the evolution function of the internal energy that must vanish in VNR scheme
     [v, e] formulation
     """
-    def computeFunctionAndDerivative(self, var_value, mask):
-        nrj = var_value[mask]
+        _mask = np.where(mask)
+        nrj = var_value[_mask]
         eos = self._variables['EquationOfState']
+        old_rho = self._variables['OldDensity'][_mask]
+        new_rho = self._variables['NewDensity'][_mask]
+        new_spec_vol = 1. / new_rho
+        pressure = self._variables['Pressure'][_mask]
+        old_nrj = self._variables['OldEnergy'][_mask]
         p_i = np.ndarray(nrj.shape, dtype=np.float64, order='C')
         dpsurde = np.ndarray(nrj.shape, dtype=np.float64, order='C')
         eos.solve_volume_energy(new_spec_vol, nrj, p_i, dpsurde)
