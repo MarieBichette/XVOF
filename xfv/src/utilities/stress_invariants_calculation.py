4acee2c23e1dae83d93bdfacf90e5e523bb5fba4
hippo91 <guillaume.peillex@gmail.com>
2020-05-10 18:32:33 +0200

    Rewrite compute_second_invariant by making the assumption the argument is a deviatoric stress tensor. Do not return the sqrt but let the caller do the sqrt

--- xfv/src/utilities/stress_invariants_calculation.py
+++ xfv/src/utilities/stress_invariants_calculation.py
@@ -18,17 +18,11 @@ def compute_trace(stress: np.array):
 
 def compute_second_invariant(stress: np.array):
     """
-    Compute the second invariant of stress tensor
+    Compute the square of the second invariant of stress tensor
     J2 = sqrt(3/2 S:S) where S is the deviatoric part of stress tensor
-    :param stress : stress tensor
+    :param stress : deviatoric stress tensor
     """
-    trace = compute_trace(stress)
-    # Compute the deviatoric stress
-    dev_stress = np.copy(stress)
-    dev_stress[:, 0] -= 1. / 3. * trace
-    dev_stress[:, 1] -= 1. / 3. * trace
-    dev_stress[:, 2] -= 1. / 3. * trace
+    dev_stress = stress
     second_invariant = dev_stress[:, 0]**2. + dev_stress[:, 1]**2 + dev_stress[:, 2]**2
     second_invariant *= 3./2.
-    second_invariant = np.sqrt(second_invariant)
     return second_invariant
