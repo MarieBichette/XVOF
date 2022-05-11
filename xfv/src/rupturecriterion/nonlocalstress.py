95501b0d99af50fff2ce5d9a40195e9bca96b68b
MarieBichette <marie.gorecki@gmail.com>
2022-05-11 15:09:44 +0200

    Réécriture pour optimisation

--- xfv/src/rupturecriterion/nonlocalstress.py
+++ xfv/src/rupturecriterion/nonlocalstress.py
@@ -4,28 +4,38 @@
 Implementing a non local criterion for failure
 """
 import numpy as np
+from xfv.src.mass_matrix.mass_matrix_utilities import SymNDArray
 from xfv.src.rupturecriterion.rupturecriterion import RuptureCriterion
 
 
-def compute_neighbour(cells, i, radius):
+def compute_weight(cells, weight_strategy):
     """
-    Return a mask of cells located in the neighbourhood of the target i
+    Return an array containing the weight (between 0 and 1)
+    Index [i, j] : weight for cell j with respect to cell i
     """
-    distance_cell_to_i, enr_distance_cell_to_i = compute_distance(cells, i)
-    cells_in_radius = (distance_cell_to_i < radius).flatten()
-    enr_cells_in_radius = (enr_distance_cell_to_i < radius).flatten()
-    # Only enriched cells count in the computation of the "enriched" stress mean
-    enr_cells_in_radius = enr_cells_in_radius * cells.enriched
-    return cells_in_radius, enr_cells_in_radius
+    mask = cells.cell_in_target
+    coord = cells.coordinates_x[mask]
+    size = len(coord)  # nombre de mailles dans la cible
+    weight_matrix = SymNDArray((size, size), dtype=np.float64)  # matrice symétrique par construction
 
+    enr_coord = np.copy(coord)
+    mask_enriched = cells.enriched[mask]
+    enr_coord[mask_enriched] = cells.enr_coordinates_x[mask_enriched]
+    enr_weight_matrix = SymNDArray((size, size), dtype=np.float64)  # matrice symétrique par construction
 
-def compute_distance(cells, i):
-    """
-    Return the distance between cells and target i
-    """
-    distance_cell_to_i = np.abs(cells.coordinates_x - cells.coordinates_x[i])
-    enr_distance_cell_to_i = np.abs(cells.coordinates_x - cells.enr_coordinates_x[i])
-    return distance_cell_to_i, enr_distance_cell_to_i
+    # On remplit le triangle superieur de la matrice, et par construction, le triangle inférieur se remplit aussi
+    # Le calcul enrichi = même calcul sauf que l'on prend les coordonnées enrichies au lieu des coordonnées classiques
+    # (si la maille est classique, on prendra deux fois la partie classique en compte)
+    for i in range(1, size):
+        weight_matrix[:i, i] = weight_strategy.compute_weight(np.abs(coord[:i] - coord[i]))
+        enr_weight_matrix[:i, i] = weight_strategy.compute_weight(np.abs(enr_coord[:i] - enr_coord[i]))
+        weight_matrix[i, i] = 1.
+        enr_weight_matrix[i, i] = 1.
+    # Reste le poids de la maille 0 par rapport à elle même...
+    weight_matrix[0, 0] = 1.
+    enr_weight_matrix[0, 0] = 1.
+
+    return weight_matrix, enr_weight_matrix
 
 
 class NonLocalStressCriterion(RuptureCriterion):  # pylint: disable=too-few-public-methods
@@ -35,23 +45,22 @@ class NonLocalStressCriterion(RuptureCriterion):  # pylint: disable=too-few-publ
     def __init__(self, value, radius, average_strategy):
         super().__init__()
         self.critical_value = value
-        self.radius = radius
-        self.average_strategy = average_strategy
+        self.weight_strategy = average_strategy
+        self.weight_strategy.set_radius(radius)
 
     def check_criterion(self, cells, *args, **kwargs):
         """
         Check of the rupture criterion on the cells in arguments
         :param cells: cells on which to check the criterion
         """
-        mean_stress = np.zeros(cells.number_of_cells)
-        nbr_div = np.zeros(cells.number_of_cells)
+        weight_matrix, enr_weight_matrix = compute_weight(cells, self.weight_strategy)
 
-        for i in range(cells.number_of_cells):
-            distance_to_cell_i, enr_distance_to_cell_i = compute_distance(cells, i)
-            weight_i = self.average_strategy.compute_weight(self.radius, distance_to_cell_i)
-            enr_weight_i = self.average_strategy.compute_weight(self.radius, enr_distance_to_cell_i)
-            # weight_i et #enr_weight_i are 0 for cells outside the neighbourhood
-            mean_stress[i] = np.sum(cells.stress_xx * weight_i + cells.enr_stress_xx * enr_weight_i)
-            nbr_div[i] = np.sum(weight_i + enr_weight_i)
-        mean_stress = mean_stress / nbr_div
+        stress = cells.stress_xx[cells.cell_in_target]
+        enr_stress = cells.enr_stress_xx[cells.cell_in_target]
+
+        mean_stress = np.dot(weight_matrix, stress)
+        enr_mean_stress = np.dot(enr_weight_matrix, enr_stress)
+        nbr_div = np.sum(weight_matrix, axis=0) + np.sum(enr_weight_matrix, axis=0)
+
+        mean_stress = (mean_stress + enr_mean_stress) / nbr_div
         return mean_stress >= self.critical_value
